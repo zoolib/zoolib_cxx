b5b43249d3c9235ae46e1b0935cbeef81df6bedd
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2008-05-17 20:50:05 +0000

    Big changes. Represent many more SPI suites, to make it easier to conditionalize code on smaller subsets of functionality (Cocoa on Windows for example).

--- zoolib/ZCONFIG_SPI.h
+++ zoolib/ZCONFIG_SPI.h
@@ -25,8 +25,35 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "ZCONFIG_SPI_Definitions.h"
 #include "ZCONFIG_SPI_Choices.h"
 
+
 // =================================================================================================
-// boost
+#pragma mark BeOS
+#ifndef ZCONFIG_SPI_Avail__BeOS
+#	define ZCONFIG_SPI_Avail__BeOS 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__BeOS
+#	define ZCONFIG_SPI_Desired__BeOS 1
+#endif
+
+
+// =================================================================================================
+// BerkeleyDB
+#ifndef ZCONFIG_SPI_Avail__BerkeleyDB
+#	define ZCONFIG_SPI_Avail__BerkeleyDB (ZCONFIG_SPI_Avail__POSIX && ZCONFIG_SPI_Desired__POSIX)
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__BerkeleyDB
+#	define ZCONFIG_SPI_Avail__BerkeleyDB 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__BerkeleyDB
+#	define ZCONFIG_SPI_Desired__BerkeleyDB 1
+#endif
+
+
+// =================================================================================================
+#pragma mark boost
 #ifndef ZCONFIG_SPI_Avail__boost
 #	define ZCONFIG_SPI_Avail__boost 0
 #endif
@@ -37,7 +64,27 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// bzip2
+#pragma mark BSD
+#ifndef ZCONFIG_SPI_Avail__BSD
+#	if defined(__APPLE__) \
+	|| defined(__NetBSD__) \
+	|| defined(__FreeBSD__) \
+	|| defined(__OpenBSD__)
+#		define ZCONFIG_SPI_Avail__BSD 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__BSD
+#	define ZCONFIG_SPI_Avail__BSD 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__BSD
+#	define ZCONFIG_SPI_Desired__BSD 1
+#endif
+
+
+// =================================================================================================
+#pragma mark bzip2
 #ifndef ZCONFIG_SPI_Avail__bzip2
 #	define ZCONFIG_SPI_Avail__bzip2 0
 #endif
@@ -46,10 +93,129 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #	define ZCONFIG_SPI_Desired__bzip2 1
 #endif
 
+
 // =================================================================================================
-// iconv
+#pragma mark Carbon
+#ifndef ZCONFIG_SPI_Avail__Carbon
+#	if __MACH__
+#		define ZCONFIG_SPI_Avail__Carbon 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__Carbon
+#	define ZCONFIG_SPI_Avail__Carbon 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__Carbon
+#	define ZCONFIG_SPI_Desired__Carbon 1
+#endif
+
+
+// =================================================================================================
+#pragma mark CFType
+#ifndef ZCONFIG_SPI_Avail__CFType
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__CFType 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__CFType
+#	define ZCONFIG_SPI_Avail__CFType 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__CFType
+#	define ZCONFIG_SPI_Desired__CFType 1
+#endif
+
+
+// =================================================================================================
+#pragma mark Cocoa
+#ifndef ZCONFIG_SPI_Avail__Cocoa
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__Cocoa 1
+#	elif defined(__COCOTRON__)
+#		define ZCONFIG_SPI_Avail__Cocoa 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__Cocoa
+#	define ZCONFIG_SPI_Avail__Cocoa 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__Cocoa
+#	define ZCONFIG_SPI_Desired__Cocoa 1
+#endif
+
+
+// =================================================================================================
+#pragma mark CoreFoundation
+#ifndef ZCONFIG_SPI_Avail__CoreFoundation
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__CoreFoundation 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__CoreFoundation
+#	define ZCONFIG_SPI_Avail__CoreFoundation 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__CoreFoundation
+#	define ZCONFIG_SPI_Desired__CoreFoundation 1
+#endif
+
+
+// =================================================================================================
+#pragma mark CoreGraphics
+#ifndef ZCONFIG_SPI_Avail__CoreGraphics
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__CoreGraphics 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__CoreGraphics
+#	define ZCONFIG_SPI_Avail__CoreGraphics 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__CoreGraphics
+#	define ZCONFIG_SPI_Desired__CoreGraphics 1
+#endif
+
+
+// =================================================================================================
+#pragma mark DNSSD (aka ZeroConf)
+#ifndef ZCONFIG_SPI_Avail__DNSSD
+//#	define ZCONFIG_SPI_Avail__DNSSD ZCONFIG_SPI_Enabled(POSIX)
+#	define ZCONFIG_SPI_Avail__DNSSD (ZCONFIG_SPI_Avail__POSIX && ZCONFIG_SPI_Desired__POSIX)
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__DNSSD
+#	define ZCONFIG_SPI_Avail__DNSSD 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__DNSSD
+#	define ZCONFIG_SPI_Desired__DNSSD 1
+#endif
+
+
+// =================================================================================================
+// GDI
+#ifndef ZCONFIG_SPI_Avail__GDI
+#	define ZCONFIG_SPI_Avail__GDI (ZCONFIG_SPI_Avail__Win && ZCONFIG_SPI_Desired__Win)
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__GDI
+#	define ZCONFIG_SPI_Avail__GDI 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__GDI
+#	define ZCONFIG_SPI_Desired__GDI 1
+#endif
+
+
+// =================================================================================================
+#pragma mark iconv
 #ifndef ZCONFIG_SPI_Avail__iconv
-#	define ZCONFIG_SPI_Avail__iconv 0
+#	define ZCONFIG_SPI_Avail__iconv (ZCONFIG_SPI_Avail__MacOSX && ZCONFIG_SPI_Desired__MacOSX)
 #endif
 
 #ifndef ZCONFIG_SPI_Desired__iconv
@@ -58,7 +224,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// ICU
+#pragma mark ICU
 #ifndef ZCONFIG_SPI_Avail__ICU
 #	define ZCONFIG_SPI_Avail__ICU 0
 #endif
@@ -69,7 +235,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// imagehlp
+#pragma mark imagehlp
 #ifndef ZCONFIG_SPI_Avail__imagehlp
 #	define ZCONFIG_SPI_Avail__imagehlp 0
 #endif
@@ -80,7 +246,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// JPEGLib
+#pragma mark JPEGLib
 #ifndef ZCONFIG_SPI_Avail__JPEGLib
 #	define ZCONFIG_SPI_Avail__JPEGLib 0
 #endif
@@ -91,7 +257,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// libpng
+#pragma mark libpng
 #ifndef ZCONFIG_SPI_Avail__libpng
 #	define ZCONFIG_SPI_Avail__libpng 0
 #endif
@@ -102,7 +268,52 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// openssl
+#pragma mark Linux
+#ifndef ZCONFIG_SPI_Avail__Linux
+#	ifdef linux
+#		define ZCONFIG_SPI_Avail__Linux 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__Linux
+#	define ZCONFIG_SPI_Avail__Linux 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__Linux
+#	define ZCONFIG_SPI_Desired__Linux 1
+#endif
+
+
+// =================================================================================================
+#pragma mark MacClassic
+#ifndef ZCONFIG_SPI_Avail__MacClassic
+#	define ZCONFIG_SPI_Avail__MacClassic 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__MacClassic
+#	define ZCONFIG_SPI_Desired__MacClassic 1
+#endif
+
+
+// =================================================================================================
+#pragma mark MacOSX
+#ifndef ZCONFIG_SPI_Avail__MacOSX
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__MacOSX 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__MacOSX
+#	define ZCONFIG_SPI_Avail__MacOSX 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__MacOSX
+#	define ZCONFIG_SPI_Desired__MacOSX 1
+#endif
+
+
+// =================================================================================================
+#pragma mark openssl
 #ifndef ZCONFIG_SPI_Avail__openssl
 #	define ZCONFIG_SPI_Avail__openssl 0
 #endif
@@ -113,7 +324,59 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// stlsoft
+#pragma mark POSIX
+#ifndef ZCONFIG_SPI_Avail__POSIX
+#	if defined(__MACH__)
+#		define ZCONFIG_SPI_Avail__POSIX 1
+#	elif defined(linux)
+#		define ZCONFIG_SPI_Avail__POSIX 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__POSIX
+#	define ZCONFIG_SPI_Avail__POSIX 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__POSIX
+#	define ZCONFIG_SPI_Desired__POSIX 1
+#endif
+
+
+// =================================================================================================
+#pragma mark pthread
+#ifndef ZCONFIG_SPI_Avail__pthread
+//#	define ZCONFIG_SPI_Avail__pthread (ZCONFIG_SPI_Enabled(POSIX))
+#	define ZCONFIG_SPI_Avail__pthread (ZCONFIG_SPI_Avail__POSIX && ZCONFIG_SPI_Desired__POSIX)
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__pthread
+#	define ZCONFIG_SPI_Avail__pthread 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__pthread
+#	define ZCONFIG_SPI_Desired__pthread 1
+#endif
+
+
+// =================================================================================================
+// QuickDraw
+#ifndef ZCONFIG_SPI_Avail__QuickDraw
+#	define ZCONFIG_SPI_Avail__QuickDraw \
+	((ZCONFIG_SPI_Avail__Carbon && ZCONFIG_SPI_Desired__Carbon) \
+	|| (ZCONFIG_SPI_Avail__MacClassic && ZCONFIG_SPI_Desired__MacClassic))
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__QuickDraw
+#	define ZCONFIG_SPI_Avail__QuickDraw 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__QuickDraw
+#	define ZCONFIG_SPI_Desired__QuickDraw 1
+#endif
+
+
+// =================================================================================================
+#pragma mark stlsoft
 #ifndef ZCONFIG_SPI_Avail__stlsoft
 #	define ZCONFIG_SPI_Avail__stlsoft 0
 #endif
@@ -124,7 +387,44 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 // =================================================================================================
-// zlib
+#pragma mark Win
+#ifndef ZCONFIG_SPI_Avail__Win
+#	if defined(WIN32)
+#		define ZCONFIG_SPI_Avail__Win 1
+#	elif defined(WINDOWS)
+#		define ZCONFIG_SPI_Avail__Win 1
+#	elif __MWERKS__ && __INTEL__
+#		define ZCONFIG_SPI_Avail__Win 1
+#	endif
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__Win
+#	define ZCONFIG_SPI_Avail__Win 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__Win
+#	define ZCONFIG_SPI_Desired__Win 1
+#endif
+
+
+// =================================================================================================
+#pragma mark X11
+#ifndef ZCONFIG_SPI_Avail__X11
+//#	define ZCONFIG_SPI_Avail__X11 (ZCONFIG_SPI_Enabled(POSIX))
+#	define ZCONFIG_SPI_Avail__X11 (ZCONFIG_SPI_Avail__POSIX && ZCONFIG_SPI_Desired__POSIX)
+#endif
+
+#ifndef ZCONFIG_SPI_Avail__X11
+#	define ZCONFIG_SPI_Avail__X11 0
+#endif
+
+#ifndef ZCONFIG_SPI_Desired__X11
+#	define ZCONFIG_SPI_Desired__X11 1
+#endif
+
+
+// =================================================================================================
+#pragma mark zlib
 #ifndef ZCONFIG_SPI_Avail__zlib
 #	define ZCONFIG_SPI_Avail__zlib 0
 #endif
