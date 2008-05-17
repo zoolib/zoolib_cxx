8a22083c6fd129283ea93d42949fdf5805c28aae
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2008-05-17 20:58:03 +0000

    Whitespace, and spurious forward declaration.

--- zoolib/ZTQL_Condition.h
+++ zoolib/ZTQL_Condition.h
@@ -28,8 +28,6 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <set>
 
-class ZTSpec;
-
 // =================================================================================================
 #pragma mark -
 #pragma mark * ZTQL::ComparatorRep
@@ -261,6 +259,7 @@ public:
 private:
 	ZRef<ComparandRep> fRep;
 	};
+
 } // namespace ZTQL
 
 // =================================================================================================
