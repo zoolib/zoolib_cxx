a660c089f1200440c1734c2e6e66403bf4029939
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2008-11-25 03:27:38 +0000

    The ZYad_ML's most important component is ZYadListMapR_ML, which treats a ZML::Reader as a ListMap, where the tag name is returned as an entry name, text appears under entries with empty names.

---
+++ zoolib/ZYad_ML.cpp
@@ -0,0 +1,151 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2008 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zoolib/ZYad_ML.h"
+
+using std::string;
+using std::vector;
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * Static helpers
+
+static void sThrowParseException(const string& iMessage)
+	{
+	throw ZYadParseException_ML(iMessage);
+	}
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadParseException_ML
+
+ZYadParseException_ML::ZYadParseException_ML(const string& iWhat)
+:	ZYadParseException(iWhat)
+	{}
+
+ZYadParseException_ML::ZYadParseException_ML(const char* iWhat)
+:	ZYadParseException(iWhat)
+	{}
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadPrimR_ML
+
+ZYadPrimR_ML::ZYadPrimR_ML(const string& iString)
+:	ZYadR_TValue(iString)
+	{}
+
+void ZYadPrimR_ML::Finish()
+	{}
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadListMapR_ML
+
+ZYadListMapR_ML::ZYadListMapR_ML(ZML::Reader& iR)
+:	fR(iR),
+	fPosition(0)
+	{}
+
+ZYadListMapR_ML::ZYadListMapR_ML(
+	ZML::Reader& iR, const string& iTagName, const ZTuple& iAttrs)
+:	fR(iR),
+	fTagName(iTagName),
+	fAttrs(iAttrs),
+	fPosition(0)
+	{}
+
+void ZYadListMapR_ML::Finish()
+	{
+	if (!fTagName.empty())
+		{
+		if (!sSkip(fR, fTagName))
+			sThrowParseException("Expected end tag '" + fTagName + "'");
+		fTagName.clear();
+		}
+	}
+
+bool ZYadListMapR_ML::HasChild()
+	{
+	this->pMoveIfNecessary();
+	return fValue_Current;
+	}
+
+ZRef<ZYadR> ZYadListMapR_ML::NextChild()
+	{
+	this->pMoveIfNecessary();
+
+	if (fValue_Current)
+		{
+		fValue_Prior = fValue_Current;
+		fValue_Current.Clear();
+		++fPosition;
+		}
+
+	return fValue_Prior;
+	}
+
+size_t ZYadListMapR_ML::GetPosition()
+	{ return fPosition; }
+
+string ZYadListMapR_ML::Name()
+	{ return fChildName; }
+
+ZTuple ZYadListMapR_ML::GetAttrs()
+	{ return fAttrs; }
+
+void ZYadListMapR_ML::pMoveIfNecessary()
+	{
+	if (fValue_Current)
+		return;
+
+	if (fValue_Prior)
+		{
+		fValue_Prior->Finish();
+		fValue_Prior.Clear();
+		}
+
+	fChildName = fR.Name();
+	switch (fR.Current())
+		{
+		case ZML::eToken_TagBegin:
+			{
+			ZTuple theAttrs = fR.Attrs();
+			fR.Advance();
+			fValue_Current = new ZYadListMapR_ML(fR, fChildName, theAttrs);
+			break;
+			}
+		case ZML::eToken_TagEmpty:
+			{
+			ZTuple theAttrs = fR.Attrs();
+			fR.Advance();
+			fValue_Current = new ZYadListMapR_ML(fR, "", theAttrs);
+			break;
+			}
+		case ZML::eToken_Text:
+			{
+			string theString;
+			ZStrimW_String(theString).CopyAllFrom(fR.Text());
+			fR.Advance();
+			fValue_Current = new ZYadPrimR_ML(theString);
+			break;
+			}
+		}
+	}
---
+++ zoolib/ZYad_ML.h
@@ -0,0 +1,109 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2008 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZYad_ML__
+#define __ZYad_ML__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZML.h"
+#include "zoolib/ZYad_ZooLib.h"
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadParseException_ML
+
+class ZYadParseException_ML : public ZYadParseException
+	{
+public:
+	ZYadParseException_ML(const std::string& iWhat);
+	ZYadParseException_ML(const char* iWhat);
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadR_ML
+
+class ZYadR_ML : public virtual ZYadR
+	{
+public:
+	// Our protocol
+	virtual void Finish() = 0;
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZYadPrimR_ML
+
+class ZYadPrimR_ML
+:	public ZYadR_TValue,
+	public ZYadPrimR,
+	public ZYadR_ML
+	{
+public:
+	ZYadPrimR_ML(const std::string& iString);
+
+// From ZYadR_ML
+	virtual void Finish();
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * YadListMapR_ML
+
+class ZYadListMapR_ML
+:	public ZYadListMapR,
+	public ZYadR_ML
+	{
+public:
+	ZYadListMapR_ML(ZML::Reader& iR);
+	ZYadListMapR_ML(ZML::Reader& iR, const std::string& iTagName, const ZTuple& iAttrs);
+
+// From ZYadR_ML
+	virtual void Finish();
+
+// From ZYadListR and ZYadMapR via ZYadListMapR
+	virtual bool HasChild();
+	virtual ZRef<ZYadR> NextChild();
+
+// From ZYadListR via ZYadListMapR
+	virtual size_t GetPosition();
+
+// From ZYadMapR via ZYadListMapR
+	virtual std::string Name();
+
+// Our protocol
+	ZTuple GetAttrs();
+
+private:
+	void pMoveIfNecessary();
+
+	ZML::Reader& fR;
+
+	ZRef<ZYadR_ML> fValue_Current;
+	ZRef<ZYadR_ML> fValue_Prior;
+
+	std::string fTagName;
+	ZTuple fAttrs;
+	size_t fPosition;
+	bool fExhausted;
+	std::string fChildName;
+	};
+
+#endif // __ZYad_ML__
