4390b0a0b3048f96a04cc4a295b5952f795a9ddf
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2008-12-14 16:17:07 +0000

    Add sInvokeOnMainThread.

--- zoolib/ZUtil_CarbonEvents.cpp
+++ zoolib/ZUtil_CarbonEvents.cpp
@@ -20,11 +20,16 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ZUtil_CarbonEvents.h"
 
+#include "zoolib/ZThreadImp.h"
+#include "zoolib/ZTypes.h"
+
+#if ZCONFIG_SPI_Enabled(Carbon)
+
+#include ZMACINCLUDE3(Carbon,HIToolbox,CarbonEvents.h)
+
 // =================================================================================================
 #pragma mark -
 #pragma mark * ZUtil_CarbonEvents
-
-#if ZCONFIG_SPI_Enabled(Carbon)
 
 bool ZUtil_CarbonEvents::sGetParam(EventRef iEventRef, EventParamName iName, EventParamType iType,
 	size_t iBufferSize, void* iBuffer)
@@ -39,6 +44,130 @@ size_t ZUtil_CarbonEvents::sGetParamLength(
 	if (noErr == ::GetEventParameter(iEventRef, iName, iType, nil, 0, &theLength, nil))
 		return theLength;
 	return 0;
+	}
+
+void ZUtil_CarbonEvents::sSetParam(EventRef iEventRef, EventParamName iName, EventParamType iType,
+	size_t iBufferSize, const void* iBuffer)
+	{
+	::SetEventParameter(iEventRef, iName, iType, iBufferSize, iBuffer);
+	}
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * Handler
+
+namespace {
+
+using namespace ZUtil_CarbonEvents;
+
+static const UInt32 kEventClassID_ZooLib = 'ZooL';
+static const UInt32 kEventKindID_Call = 'Call';
+
+class Handler
+	{
+public:
+	Handler();
+	~Handler();
+
+	void InvokeOnMainThread(Callback_t iCallback, void* iRefcon);
+
+	static EventHandlerUPP sEventHandlerUPP;
+	static pascal OSStatus sEventHandler(EventHandlerCallRef iCallRef, EventRef iEventRef, void* iRefcon);
+	OSStatus EventHandler(EventHandlerCallRef iCallRef, EventRef iEventRef);
+
+	EventQueueRef fEventQueueRef;
+	EventHandlerRef fEventHandlerRef;
+	ZThreadImp::ID fMainID;
+	};
+
+struct Context_t
+	{
+	Callback_t fCallback;
+	void* fRefcon;
+	};
+
+Handler::Handler()
+	{
+	fMainID = ZThreadImp::sID();
+
+	fEventQueueRef = ::GetMainEventQueue();
+
+	static const EventTypeSpec sEvents[] =
+		{
+		{ kEventClassID_ZooLib, kEventKindID_Call }
+		};
+
+	::InstallEventHandler(::GetEventDispatcherTarget(), sEventHandlerUPP,
+		countof(sEvents), sEvents,
+		this, &fEventHandlerRef);
+	}
+
+Handler::~Handler()
+	{
+	if (fEventHandlerRef)
+		::RemoveEventHandler(fEventHandlerRef);
+	fEventHandlerRef = nil;
+
+	fEventQueueRef = nil;
+	}
+
+void Handler::InvokeOnMainThread(Callback_t iCallback, void* iRefcon)
+	{
+	if (fMainID == ZThreadImp::sID())
+		{
+		iCallback(iRefcon);
+		}
+	else
+		{
+		EventRef theEventRef;
+		::MacCreateEvent(nil, kEventClassID_ZooLib, kEventKindID_Call,
+			::GetCurrentEventTime(), kEventAttributeNone, &theEventRef);
+
+		Context_t theContext;
+		theContext.fCallback = iCallback;
+		theContext.fRefcon = iRefcon;
+		sSetParam_T(theEventRef, 'Cont', typeData, theContext);
+
+		::PostEventToQueue(fEventQueueRef, theEventRef, kEventPriorityStandard);
+		::ReleaseEvent(theEventRef);
+		}
+	}
+
+EventHandlerUPP Handler::sEventHandlerUPP = NewEventHandlerUPP(sEventHandler);
+
+pascal OSStatus Handler::sEventHandler(
+	EventHandlerCallRef iCallRef, EventRef iEventRef, void* iRefcon)
+	{ return static_cast<Handler*>(iRefcon)->EventHandler(iCallRef, iEventRef); }
+
+OSStatus Handler::EventHandler(EventHandlerCallRef iCallRef, EventRef iEventRef)
+	{
+	switch (::GetEventClass(iEventRef))
+		{
+		case kEventClassID_ZooLib:
+			{
+			switch (::GetEventKind(iEventRef))
+				{
+				case kEventKindID_Call:
+					{
+					Context_t theContext;
+					if (sGetParam_T(iEventRef, 'Cont', typeData, theContext))
+						theContext.fCallback(theContext.fRefcon);
+					return noErr;
+					}
+				}
+			break;
+			}
+		}
+	return eventNotHandledErr;
+	}
+
+Handler sHandler;
+
+} // anonymous namespace
+
+void ZUtil_CarbonEvents::sInvokeOnMainThread(Callback_t iCallback, void* iRefcon)
+	{
+	sHandler.InvokeOnMainThread(iCallback, iRefcon);
 	}
 
 #endif // ZCONFIG_SPI_Enabled(Carbon)
--- zoolib/ZUtil_CarbonEvents.h
+++ zoolib/ZUtil_CarbonEvents.h
@@ -29,7 +29,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #if ZCONFIG_SPI_Enabled(Carbon)
 
-#include ZMACINCLUDE(Carbon,Carbon.h)
+#include ZMACINCLUDE3(Carbon,HIToolbox,CarbonEventsCore.h)
 
 namespace ZUtil_CarbonEvents {
 
@@ -53,7 +53,43 @@ T sGetParam_T(EventRef iEventRef, EventParamName iName, EventParamType iType)
 	return T();
 	}
 
+void sSetParam(EventRef iEventRef, EventParamName iName, EventParamType iType,
+	size_t iBufferSize, const void* iBuffer);
+
+template <typename T>
+void sSetParam_T(EventRef iEventRef, EventParamName iName, EventParamType iType, const T& iParam)
+	{
+	sSetParam(iEventRef, iName, iType, sizeof(T), &iParam);
+	}
+
+typedef void (*Callback_t)(void* iRefcon);
+
+void sInvokeOnMainThread(Callback_t iCallback, void* iRefcon);
+
 } // namespace ZUtil_CarbonEvents
+
+#if defined(UNIVERSAL_INTERFACES_VERSION) && UNIVERSAL_INTERFACES_VERSION <= 0x0341
+// Taken from Universal Headers 3.4.2
+enum {
+
+  /*
+   * This event parameter may be added to any event that is posted to
+   * the main event queue. When the event is removed from the queue and
+   * sent to the event dispatcher, the dispatcher will retrieve the
+   * EventTargetRef contained in this parameter and send the event
+   * directly to that event target. If this parameter is not available
+   * in the event, the dispatcher will send the event to a suitable
+   * target, or to the application target if no more specific target is
+   * appropriate. Available in CarbonLib 1.3.1 and later, and Mac OS X.
+   */
+  kEventParamPostTarget         = FOUR_CHAR_CODE('ptrg'), /* typeEventTargetRef*/
+
+  /*
+   * Indicates an event parameter of type EventTargetRef.
+   */
+  typeEventTargetRef            = FOUR_CHAR_CODE('etrg') /* EventTargetRef*/
+};
+#endif
 
 #endif // ZCONFIG_SPI_Enabled(Carbon)
 
