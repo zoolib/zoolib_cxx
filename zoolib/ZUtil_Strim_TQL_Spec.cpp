2ad40778ea2e7efc44046f6573ea148e8cb469e8
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-03-15 19:32:28 +0000

    Don't need this #include.

--- zoolib/ZUtil_Strim_TQL_Spec.cpp
+++ zoolib/ZUtil_Strim_TQL_Spec.cpp
@@ -21,7 +21,6 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "zoolib/ZUtil_Strim_TQL_Spec.h"
 
 #include "zoolib/ZTQL_LogOp.h"
-#include "zoolib/ZStrimW_Escapify.h"
 #include "zoolib/ZUtil_Strim_Tuple.h"
 
 NAMESPACE_ZOOLIB_BEGIN
