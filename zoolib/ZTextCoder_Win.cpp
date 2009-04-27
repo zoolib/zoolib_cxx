679754c5ba2837455e3b8829579e9e2ba80478e9
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-04-27 17:03:29 +0000

    stdexcept is no longer picked up from other headers.

--- zoolib/ZTextCoder_Win.cpp
+++ zoolib/ZTextCoder_Win.cpp
@@ -26,6 +26,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "zoolib/ZThread.h"
 
 #include <map>
+#include <stdexcept>
 
 using std::map;
 using std::min;
