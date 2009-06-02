29493aca4b18484c687231f2a39f147a1c3c35ec
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-06-02 02:21:51 +0000

    x86_64 support.

--- zoolib/zconfigd.h
+++ zoolib/zconfigd.h
@@ -33,6 +33,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define ZCONFIG_Processor_Sparc 8
 #define ZCONFIG_Processor_Alpha 16
 #define ZCONFIG_Processor_ARM 32
+#define ZCONFIG_Processor_x86_64 64
 
 // Byte order -- ZCONFIG_Endian
 #define ZCONFIG_Endian_Big 1
