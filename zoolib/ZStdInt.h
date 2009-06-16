73344254de581feaa1e0c5f2a36654630be86972
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-06-16 21:30:19 +0000

    Change indentation to make clear what's included.

--- zoolib/ZStdInt.h
+++ zoolib/ZStdInt.h
@@ -43,96 +43,96 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #else // defined(ZProjectHeader_StdInt)
 
-#include "zoolib/ZCONFIG_SPI.h"
+	#include "zoolib/ZCONFIG_SPI.h"
 
-#if ZCONFIG_SPI_Enabled(MacOSX) \
-	|| ZCONFIG_SPI_Enabled(MacClassic) \
-	|| ZCONFIG_SPI_Enabled(Carbon)
+	#if ZCONFIG_SPI_Enabled(MacOSX) \
+		|| ZCONFIG_SPI_Enabled(MacClassic) \
+		|| ZCONFIG_SPI_Enabled(Carbon)
 
-	#if __arm__
-		#include <MacTypes.h>
-	#else
-		#include ZMACINCLUDE3(CoreServices,CarbonCore,MacTypes.h)
+		#if __arm__
+			#include <MacTypes.h>
+		#else
+			#include ZMACINCLUDE3(CoreServices,CarbonCore,MacTypes.h)
+		#endif
+
+		// Doing this makes a lot of stuff more directly compatible on Mac.
+		#ifndef _UINT32
+			#define _UINT32 1
+			typedef UInt32 uint32;
+		#endif
+
 	#endif
 
-	// Doing this makes a lot of stuff more directly compatible on Mac.
+	#if ZCONFIG(Compiler, MSVC)
+
+		#ifndef _INT8_T
+			#define _INT8_T
+			typedef	__int8 int8_t;
+		#endif
+
+		#ifndef _INT16_T
+			#define _INT16_T
+			typedef	__int16 int16_t;
+		#endif
+
+		#ifndef _INT32_T
+			#define _INT32_T
+			typedef	__int32 int32_t;
+		#endif
+
+		#ifndef _INT64_T
+			#define _INT64_T
+			typedef	__int64 int64_t;
+		#endif
+
+		#ifndef _UINT8_T_DECLARED
+			#define _UINT8_T_DECLARED
+			typedef unsigned __int8 uint8_t;
+		#endif
+
+		#ifndef _UINT16_T_DECLARED
+			#define _UINT16_T_DECLARED
+			typedef unsigned __int16 uint16_t;
+		#endif
+
+		#ifndef _UINT32_T_DECLARED
+			#define _UINT32_T_DECLARED
+			typedef unsigned __int32 uint32_t;
+		#endif
+
+		#ifndef _UINT64_T_DECLARED
+			#define _UINT64_T_DECLARED
+			typedef unsigned __int64 uint64_t;
+		#endif
+
+	#else
+
+		#include <stdint.h>
+
+	#endif
+
+	typedef int8_t int8;
+	typedef uint8_t uint8;
+
+	typedef int16_t int16;
+	typedef uint16_t uint16;
+
+	#ifndef _INT32
+		#ifndef _MSL_USING_MW_C_HEADERS
+			#define _INT32 1
+		#endif
+		typedef int32_t int32;
+	#endif
+
 	#ifndef _UINT32
 		#define _UINT32 1
-		typedef UInt32 uint32;
+		typedef uint32_t uint32;
 	#endif
 
-#endif
+	typedef int64_t int64;
+	typedef uint64_t uint64;
 
-#if ZCONFIG(Compiler, MSVC)
-
-	#ifndef _INT8_T
-		#define _INT8_T
-		typedef	__int8 int8_t;
-	#endif
-
-	#ifndef _INT16_T
-		#define _INT16_T
-		typedef	__int16 int16_t;
-	#endif
-
-	#ifndef _INT32_T
-		#define _INT32_T
-		typedef	__int32 int32_t;
-	#endif
-
-	#ifndef _INT64_T
-		#define _INT64_T
-		typedef	__int64 int64_t;
-	#endif
-
-	#ifndef _UINT8_T_DECLARED
-		#define _UINT8_T_DECLARED
-		typedef unsigned __int8 uint8_t;
-	#endif
-
-	#ifndef _UINT16_T_DECLARED
-		#define _UINT16_T_DECLARED
-		typedef unsigned __int16 uint16_t;
-	#endif
-
-	#ifndef _UINT32_T_DECLARED
-		#define _UINT32_T_DECLARED
-		typedef unsigned __int32 uint32_t;
-	#endif
-
-	#ifndef _UINT64_T_DECLARED
-		#define _UINT64_T_DECLARED
-		typedef unsigned __int64 uint64_t;
-	#endif
-
-#else
-
-	#include <stdint.h>
-
-#endif
-
-typedef int8_t int8;
-typedef uint8_t uint8;
-
-typedef int16_t int16;
-typedef uint16_t uint16;
-
-#ifndef _INT32
-	#ifndef _MSL_USING_MW_C_HEADERS
-		#define _INT32 1
-	#endif
-	typedef int32_t int32;
-#endif
-
-#ifndef _UINT32
-	#define _UINT32 1
-	typedef uint32_t uint32;
-#endif
-
-typedef int64_t int64;
-typedef uint64_t uint64;
-
-typedef int64 bigtime_t;
+	typedef int64 bigtime_t;
 
 #endif // defined(ZStdInt_ProjectHeader)
 
