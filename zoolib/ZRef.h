46f770fa0f6e44ea2a7f8114afa843153e788cd8
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-06-16 21:23:20 +0000

    Whitespace.

--- zoolib/ZRef.h
+++ zoolib/ZRef.h
@@ -24,7 +24,6 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ZCompat_algorithm.h" // For std::swap
 #include "zoolib/ZCompat_operator_bool.h"
-
 #include "zoolib/ZDebug.h"
 
 NAMESPACE_ZOOLIB_BEGIN
