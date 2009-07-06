b27dc9d9e94e8afb8285e589b8a67c9143e989c6
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-07-06 16:43:58 +0000

    Need to include ZUnicode.h.

--- zoolib/ZTextCoder_ICU.cpp
+++ zoolib/ZTextCoder_ICU.cpp
@@ -24,6 +24,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ZDebug.h"
 #include "zoolib/ZFactoryChain.h"
+#include "zoolib/ZUnicode.h"
 
 #include <stdexcept>
 #include "unicode/ucnv.h"
