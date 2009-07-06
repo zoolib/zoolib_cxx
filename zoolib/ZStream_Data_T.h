ac922c6ef2d13b638c357743d83d5f4a06f3ddbc
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2009-07-06 18:31:35 +0000

    Create ZValData_ZooLib, and rework ZMemoryBlock so it's a typedef thereof.

---
+++ zoolib/ZStream_Data_T.h
@@ -0,0 +1,214 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2009 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZStream_Data_T__
+#define __ZStream_Data_T__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZStream.h"
+
+NAMESPACE_ZOOLIB_BEGIN
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZStreamRPos_Data_T
+
+template <class T>
+class ZStreamRPos_Data_T : public ZStreamRPos
+	{
+public:
+	ZStreamRPos_Data_T(const T& iData)
+	:	fData(iData),
+		fPosition(0)
+		{}
+	
+	~ZStreamRPos_Data_T()
+		{}
+
+// From ZStreamR via ZStreamRPos
+	virtual void Imp_Read(void* iDest, size_t iCount, size_t* oCountRead)
+		{
+		size_t countToCopy = ZStream::sClampedSize(iCount, fData.GetSize(), fPosition);
+		fData.CopyTo(fPosition, iDest, countToCopy);
+		fPosition += countToCopy;
+
+		if (oCountRead)
+			*oCountRead = countToCopy;
+		}
+
+	virtual void Imp_Skip(uint64 iCount, uint64* oCountSkipped)
+		{
+		size_t realSkip = ZStream::sClampedSize(iCount, fData.GetSize(), fPosition);
+		fPosition += realSkip;
+		if (oCountSkipped)
+			*oCountSkipped = realSkip;
+		}
+
+// From ZStreamRPos
+	virtual uint64 Imp_GetPosition()
+		{ return fPosition; }
+
+	virtual void Imp_SetPosition(uint64 iPosition)
+		{ fPosition = iPosition; }
+
+	virtual uint64 Imp_GetSize()
+		{ return fData.GetSize(); }
+
+private:
+	T fData;
+	uint64 fPosition;
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZStreamRWPos_Data_T
+
+template <class T>
+class ZStreamRWPos_Data_T : public ZStreamRWPos
+	{
+public:
+	ZStreamRWPos_Data_T(T& iData, size_t iGrowIncrement)
+	:	fData(iData)
+		{
+		fGrowIncrement = iGrowIncrement;
+		fPosition = 0;
+		fSizeLogical = fData.GetSize();
+		}
+	
+	ZStreamRWPos_Data_T(T& iData)
+	:	fData(iData)
+		{
+		fGrowIncrement = 64;
+		fPosition = 0;
+		fSizeLogical = fData.GetSize();
+		}
+
+	~ZStreamRWPos_Data_T()
+		{
+		// Finally, make sure the Data is the real size, not the potentially
+		// overallocated size we've been using
+		fData.SetSize(fSizeLogical);
+		}
+
+// From ZStreamR via ZStreamRWPos
+	virtual void Imp_Read(void* iDest, size_t iCount, size_t* oCountRead)
+		{
+		size_t countToCopy = ZStream::sClampedSize(iCount, fSizeLogical, fPosition);
+		fData.CopyTo(fPosition, iDest, countToCopy);
+		fPosition += countToCopy;
+
+		if (oCountRead)
+			*oCountRead = countToCopy;
+		}
+		
+	virtual void Imp_Skip(uint64 iCount, uint64* oCountSkipped)
+		{
+		size_t realSkip = ZStream::sClampedSize(iCount, fSizeLogical, fPosition);
+		fPosition += realSkip;
+		if (oCountSkipped)
+			*oCountSkipped = realSkip;
+		}
+
+// From ZStreamW via ZStreamRWPos
+	virtual void Imp_Write(const void* iSource, size_t iCount, size_t* oCountWritten)
+		{
+		uint64 neededSpace = fPosition + iCount;
+		if (fData.GetSize() < neededSpace)
+			{
+			uint64 realSize = std::max(neededSpace, uint64(fData.GetSize()) + fGrowIncrement);
+			if (realSize == size_t(realSize))
+				fData.SetSize(realSize);
+			}
+
+		size_t countToCopy = ZStream::sClampedSize(iCount, fData.GetSize(), fPosition);
+
+		fData.CopyFrom(fPosition, iSource, iCount);
+
+		fPosition += countToCopy;
+
+		if (fSizeLogical < fPosition)
+			fSizeLogical = fPosition;
+
+		if (oCountWritten)
+			*oCountWritten = countToCopy;
+		}
+
+	virtual void Imp_Flush()
+		{ fData.SetSize(fSizeLogical); }
+
+// From ZStreamRPos/ZStreamWPos via ZStreamRWPos
+	virtual uint64 Imp_GetPosition()
+		{ return fPosition; }
+
+	virtual void Imp_SetPosition(uint64 iPosition)
+		{ fPosition = iPosition; }
+
+	virtual uint64 Imp_GetSize()
+		{ return fSizeLogical; }
+
+// From ZStreamWPos via ZStreamRWPos
+	virtual void Imp_SetSize(uint64 iSize)
+		{
+		size_t realSize = iSize;
+		if (realSize != iSize)
+			sThrowBadSize();
+
+		fData.SetSize(realSize);
+		fSizeLogical = realSize;
+		}
+
+private:
+	T& fData;
+	size_t fGrowIncrement;
+	uint64 fPosition;
+	size_t fSizeLogical;
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * Data stream reading functions
+
+template <class T>
+void sReadAll_T(T& ioData, const ZStreamR& iStreamR)
+	{ ZStreamRWPos_Data_T<T>(ioData).CopyAllFrom(iStreamR); }
+
+template <class T>
+void sRead_T(T& ioData, const ZStreamR& iStreamR, size_t iSize)
+	{ ZStreamRWPos_Data_T<T>(ioData).CopyFrom(iStreamR, iSize); }
+
+template <class T>
+T sReadAll_T(const ZStreamR& iStreamR)
+	{
+	T theData;
+	sReadAll_T<T>(theData, iStreamR);
+	return theData;
+	}
+
+template <class T>
+T sReadAll(const ZStreamR& iStreamR, size_t iSize)
+	{
+	T theData;
+	sRead_T<T>(theData, iStreamR, iSize);
+	return theData;
+	}
+
+NAMESPACE_ZOOLIB_END
+
+#endif // __ZStream_Data_T__
---
+++ zoolib/ZValData_ZooLib.h
@@ -0,0 +1,89 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2009 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZValData_ZooLib__
+#define __ZValData_ZooLib__ 1
+
+#include "zoolib/ZCompare.h"
+#include "zoolib/ZRef_Counted.h"
+
+NAMESPACE_ZOOLIB_BEGIN
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZValData_ZooLib
+
+class ZValData_ZooLib
+	{
+    ZOOLIB_DEFINE_OPERATOR_BOOL_TYPES(ZValData_ZooLib,
+    	operator_bool_generator_type, operator_bool_type);
+	class Rep;
+
+public:
+	operator operator_bool_type() const;
+	void swap(ZValData_ZooLib& iOther);
+
+	ZValData_ZooLib();
+	ZValData_ZooLib(const ZValData_ZooLib& iOther);
+	~ZValData_ZooLib();
+	ZValData_ZooLib& operator=(const ZValData_ZooLib& iOther);
+
+	ZValData_ZooLib(size_t iSize);
+	ZValData_ZooLib(const void* iSourceData, size_t iSize);
+
+	int Compare(const ZValData_ZooLib& iOther) const;
+	bool operator<(const ZValData_ZooLib& iOther) const;
+	bool operator==(const ZValData_ZooLib& iOther) const;
+
+	size_t GetSize() const;
+	void SetSize(size_t iSize);
+
+	const void* GetData() const;
+	void* GetData();
+
+	void CopyFrom(size_t iOffset, const void* iSource, size_t iCount);
+	void CopyFrom(const void* iSource, size_t iCount);
+
+	void CopyTo(size_t iOffset, void* iDest, size_t iCount) const;
+	void CopyTo(void* iDest, size_t iCount) const;
+
+private:
+	void pTouch();
+
+	ZRef<Rep> fRep;
+	};
+
+template <> inline int sCompare_T(const ZValData_ZooLib& iL, const ZValData_ZooLib& iR)
+	{ return iL.Compare(iR); }
+
+NAMESPACE_ZOOLIB_END
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * std::swap
+
+namespace std {
+
+inline void swap(ZOOLIB_PREFIX::ZValData_ZooLib& a, ZOOLIB_PREFIX::ZValData_ZooLib& b)
+	{ a.swap(b); }
+
+} // namespace std
+
+#endif // __ZValData_ZooLib__
