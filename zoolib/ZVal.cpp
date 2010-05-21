4420540c01d3d00eb23373baf4562476d2f720bd
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-05-21 13:57:46 +0000

    Fix include statement.

--- zoolib/ZVal.cpp
+++ zoolib/ZVal.cpp
@@ -18,7 +18,7 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
-#include "ZVal.h"
+#include "zoolib/ZVal.h"
 
 NAMESPACE_ZOOLIB_BEGIN
 
