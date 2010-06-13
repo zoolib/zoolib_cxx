5bb9bdb1606323cef0bc958bc70ba7a952f2875a
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-06-13 20:13:22 +0000

    Conform to ZThreadOld movement.

--- more/zoolib/tuplebase/ZRWLock.h
+++ more/zoolib/tuplebase/ZRWLock.h
@@ -22,7 +22,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define __ZRWLock__ 1
 #include "zconfig.h"
 
-#include "zoolib/ZThreadOld.h"
+#include "zoolib/tuplebase/ZThreadOld.h"
 
 #include <vector>
 
--- more/zoolib/tuplebase/ZThreadOld.cpp
+++ more/zoolib/tuplebase/ZThreadOld.cpp
@@ -18,7 +18,7 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
-#include "zoolib/ZThreadOld.h"
+#include "zoolib/tuplebase/ZThreadOld.h"
 
 #include "zoolib/ZAtomic.h"
 #include "zoolib/ZLog.h"
