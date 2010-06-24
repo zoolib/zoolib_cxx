2b1ff6af3188873f36b3601229bf4977cd5db202
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-06-24 02:22:54 +0000

    Moved into ZWorker_Callable. --This line, and those below, will be ignored-
    M    ZTaskWorker.h

--- zoolib/ZTaskWorker.h
+++ zoolib/ZTaskWorker.h
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------------------------------
-Copyright (c) 2009 Andrew Green
+Copyright (c) 2010 Andrew Green
 http://www.zoolib.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
@@ -33,114 +33,13 @@ namespace ZooLib {
 
 class ZTaskWorker
 :	public ZTask
-,	public ZWorker
+,	public virtual ZWorker
 	{
 public:
 	ZTaskWorker(ZRef<ZTaskMaster> iTaskMaster);
 
 // From ZWorker
 	virtual void RunnerDetached();
-	};
-
-// =================================================================================================
-#pragma mark -
-#pragma mark * ZTaskWorker_T0
-
-class ZTaskWorker_T0 : public ZTaskWorker
-	{
-public:
-	typedef bool (*Function_t)(ZRef<ZTaskWorker>);
-
-	ZTaskWorker_T0(ZRef<ZTaskMaster> iTaskMaster, Function_t iFunction)
-	:	ZTaskWorker(iTaskMaster)
-	,	fFunction(iFunction)
-		{}
-
-	virtual bool Work()
-		{ return fFunction(this); }
-
-private:
-	Function_t fFunction;
-	};
-
-// =================================================================================================
-#pragma mark -
-#pragma mark * ZTaskWorker_T1
-
-template <class T0>
-class ZTaskWorker_T1 : public ZTaskWorker
-	{
-public:
-	typedef bool (*Function_t)(ZRef<ZTaskWorker>, const T0&);
-
-	ZTaskWorker_T1(ZRef<ZTaskMaster> iTaskMaster, Function_t iFunction, const T0& iT0)
-	:	ZTaskWorker(iTaskMaster)
-	,	fFunction(iFunction)
-	,	fT0(iT0)
-		{}
-
-	virtual bool Work()
-		{ return fFunction(this, fT0); }
-
-private:
-	Function_t fFunction;
-	T0 fT0;
-	};
-
-// =================================================================================================
-#pragma mark -
-#pragma mark * ZTaskWorker_T2
-
-template <class T0, class T1>
-class ZTaskWorker_T2 : public ZTaskWorker
-	{
-public:
-	typedef bool (*Function_t)(ZRef<ZTaskWorker>, const T0&, const T1&);
-
-	ZTaskWorker_T2(ZRef<ZTaskMaster> iTaskMaster,
-		Function_t iFunction, const T0& iT0, const T1& iT1)
-	:	ZTaskWorker(iTaskMaster)
-	,	fFunction(iFunction)
-	,	fT0(iT0)
-	,	fT1(iT1)
-		{}
-
-	virtual bool Work()
-		{ return fFunction(this, fT0, fT1); }
-
-private:
-	Function_t fFunction;
-	T0 fT0;
-	T1 fT1;
-	};
-
-// =================================================================================================
-#pragma mark -
-#pragma mark * ZTaskWorker_T3
-
-template <class T0, class T1, class T2>
-class ZTaskWorker_T3 : public ZTaskWorker
-	{
-public:
-	typedef bool (*Function_t)(ZRef<ZTaskWorker>, const T0& iT0, const T1&, const T2&);
-
-	ZTaskWorker_T3(ZRef<ZTaskMaster> iTaskMaster,
-		Function_t iFunction, const T0& iT0, const T1& iT1, const T2& iT2)
-	:	ZTaskWorker(iTaskMaster)
-	,	fFunction(iFunction)
-	,	fT0(iT0)
-	,	fT1(iT1)
-	,	fT2(iT2)
-		{}
-
-	virtual bool Work()
-		{ return fFunction(this, fT0, fT1, fT2); }
-
-private:
-	Function_t fFunction;
-	T0 fT0;
-	T1 fT1;
-	T2 fT2;
 	};
 
 } // namespace ZooLib
