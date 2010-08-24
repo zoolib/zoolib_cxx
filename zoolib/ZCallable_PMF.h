c9f118b2318139b15b116a2108834d74fbf416d6
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-08-24 21:17:13 +0000

    Add ZCallable_PMF4. Whitespace.

--- zoolib/ZCallable_PMF.h
+++ zoolib/ZCallable_PMF.h
@@ -136,7 +136,7 @@ MakeCallable(R (Callee_t::*iMethod)(), const Passed_t& iCallee)
 
 template <class Callee_t, class Passed_t, class R, class P0>
 class ZCallable_PMF1
-:	public ZCallable1<R, P0>
+:	public ZCallable1<R,P0>
 	{
 public:
 	typedef typename ZCallable_PMFTraits<Passed_t>::Stored_t Stored_t;
@@ -163,7 +163,7 @@ private:
 	};
 
 template <class Callee_t, class Passed_t, class R, class P0>
-ZRef<ZCallable1<R, P0> >
+ZRef<ZCallable1<R,P0> >
 MakeCallable(R (Callee_t::*iMethod)(P0), const Passed_t& iCallee)
 	{ return new ZCallable_PMF1<Callee_t, Passed_t, R, P0>(iMethod, iCallee); }
 
@@ -173,7 +173,7 @@ MakeCallable(R (Callee_t::*iMethod)(P0), const Passed_t& iCallee)
 
 template <class Callee_t, class Passed_t, class R, class P0, class P1>
 class ZCallable_PMF2
-:	public ZCallable2<R, P0, P1>
+:	public ZCallable2<R,P0,P1>
 	{
 public:
 	typedef typename ZCallable_PMFTraits<Passed_t>::Stored_t Stored_t;
@@ -200,7 +200,7 @@ private:
 	};
 
 template <class Callee_t, class Passed_t, class R, class P0, class P1>
-ZRef<ZCallable2<R, P0, P1> >
+ZRef<ZCallable2<R,P0,P1> >
 MakeCallable(R (Callee_t::*iMethod)(P0, P1), const Passed_t& iCallee)
 	{ return new ZCallable_PMF2<Callee_t, Passed_t, R, P0, P1>(iMethod, iCallee); }
 
@@ -210,7 +210,7 @@ MakeCallable(R (Callee_t::*iMethod)(P0, P1), const Passed_t& iCallee)
 
 template <class Callee_t, class Passed_t, class R, class P0, class P1, class P2>
 class ZCallable_PMF3
-:	public ZCallable3<R, P0, P1, P2>
+:	public ZCallable3<R,P0,P1,P2>
 	{
 public:
 	typedef typename ZCallable_PMFTraits<Passed_t>::Stored_t Stored_t;
@@ -237,9 +237,46 @@ private:
 	};
 
 template <class Callee_t, class Passed_t, class R, class P0, class P1, class P2>
-ZRef<ZCallable3<R, P0, P1, P2> >
+ZRef<ZCallable3<R,P0,P1,P2> >
 MakeCallable(R (Callee_t::*iMethod)(P0, P1, P2), const Passed_t& iCallee)
 	{ return new ZCallable_PMF3<Callee_t, Passed_t, R, P0, P1, P2>(iMethod, iCallee); }
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZCallable_PMF4
+
+template <class Callee_t, class Passed_t, class R, class P0, class P1, class P2, class P3>
+class ZCallable_PMF4
+:	public ZCallable4<R,P0,P1,P2,P3>
+	{
+public:
+	typedef typename ZCallable_PMFTraits<Passed_t>::Stored_t Stored_t;
+	typedef typename ZCallable_PMFTraits<Stored_t>::Temp_t Temp_t;
+
+	typedef R (Callee_t::*Method_t)(P0, P1, P2, P3);
+
+	ZCallable_PMF4(Method_t iMethod, const Passed_t& iCallee)
+	:	fMethod(iMethod)
+	,	fCallee(iCallee)
+		{}
+
+	// From ZCallable3
+	virtual R Invoke(P0 i0, P1 i1, P2 i2, P3 i3)
+		{
+		if (Temp_t temp = ZCallable_PMFTraits<Stored_t>::sGetTemp(fCallee))
+			return (ZCallable_PMFTraits<Temp_t>::sGetPtr(temp)->*fMethod)(i0, i1, i2, i3);
+		return R();
+		}
+
+private:
+	Method_t fMethod;
+	Stored_t fCallee;
+	};
+
+template <class Callee_t, class Passed_t, class R, class P0, class P1, class P2, class P3>
+ZRef<ZCallable4<R,P0,P1,P2,P3> >
+MakeCallable(R (Callee_t::*iMethod)(P0, P1, P2, P3), const Passed_t& iCallee)
+	{ return new ZCallable_PMF4<Callee_t, Passed_t, R, P0, P1, P2, P3>(iMethod, iCallee); }
 
 } // namespace ZooLib
 
