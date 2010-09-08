7ef496fd99447d97e198cabcdcca93f03c99a1e6
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-09-08 02:38:40 +0000

    Add license and conditional compilation stuff.

--- more/zoolib/iphone/ZooLib_UITableViewController.h
+++ more/zoolib/iphone/ZooLib_UITableViewController.h
@@ -1,9 +1,33 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2010 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
 #ifndef __ZooLib_UITableViewController__
 #define __ZooLib_UITableViewController__ 1
+#include "zconfig.h"
+#include "zoolib/ZCONFIG_SPI.h"
 
 #include "zoolib/ZCallable.h"
 #include "zoolib/ZQ.h"
 #include "zoolib/ZUnicodeString.h"
+
+#if ZCONFIG_SPI_Enabled(iPhone)
 
 #import <UIKit/UITableView.h>
 
@@ -149,5 +173,7 @@ using namespace ZooLib;
 -(ZRef<IPhone::UITVC_Section>)pGetSection:(size_t)iIndex;
 
 @end // ZooLib_UITableViewController
+
+#endif // ZCONFIG_SPI_Enabled(iPhone)
 
 #endif // __ZooLib_UITableViewController__
