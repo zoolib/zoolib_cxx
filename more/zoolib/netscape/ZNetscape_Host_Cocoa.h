c4b9b0792f549e9dd0d6de312ae9d9b73f15036a
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-10-16 17:28:29 +0000

    First cut at NSView-based hosting of NPAPI plugins. For now it only supports the Cocoa Event API.

---
+++ more/zoolib/netscape/ZNetscape_Host_Cocoa.h
@@ -0,0 +1,98 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2010 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZNetscape_Host_Cocoa__
+#define __ZNetscape_Host_Cocoa__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZCONFIG_SPI.h"
+
+#if defined(__OBJC__)
+#if ZCONFIG_SPI_Enabled(Cocoa)
+
+#include "zoolib/netscape/ZNetscape_Host_Std.h"
+
+#include <AppKit/NSView.h>
+
+namespace ZooLib {
+namespace ZNetscape {
+class Host_Cocoa;
+} // namespace ZNetscape
+} // namespace ZooLib
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * NSView_NetscapeHost
+
+@interface NSView_NetscapeHost : NSView
+	{
+@public
+	ZooLib::ZNetscape::Host_Cocoa* fHost;
+	}
+
+@end
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * Host_Cocoa
+
+namespace ZooLib {
+namespace ZNetscape {
+
+class Host_Cocoa : public Host_Std
+	{
+public:
+	Host_Cocoa(ZRef<GuestFactory> iGuestFactory, NSView_NetscapeHost* iView);
+	virtual ~Host_Cocoa();
+
+// From Host via Host_Std
+	virtual NPError Host_GetValue(NPP npp, NPNVariable variable, void* ret_value);
+	virtual NPError Host_SetValue(NPP npp, NPPVariable variable, void* value);
+
+	virtual void Host_InvalidateRect(NPP npp, NPRect* rect);
+	virtual void PostCreateAndLoad();
+
+	void Draw(const NSRect& iRect);
+	void FlagsChanged(NSEvent* iEvent);
+	void SendText(NSEvent* iEvent);
+	bool SendKeyEvent(NSEvent* iEvent, NPCocoaEventType iType);
+	bool SendMouseEvent(NSEvent* iEvent, NPCocoaEventType iType);
+	void FocusChanged(bool hasFocus);
+	void Timer();
+protected:
+	NSView_NetscapeHost* fView;
+
+	NPWindow fNPWindow;
+	
+	NP_CGContext fNP_CGContext;
+	NP_CGContext fNP_CGContext_Prior;
+
+	float fLeft;
+	float fTop;
+	float fRight;
+	float fBottom;
+	};
+
+} // namespace ZNetscape
+} // namespace ZooLib
+
+#endif // ZCONFIG_SPI_Enabled(Cocoa)
+#endif // defined(__OBJC__)
+#endif // __ZNetscape_Host_Cocoa__
