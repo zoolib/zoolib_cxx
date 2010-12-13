aea760a6a5870b9c9b180fd942fd0625b3033036
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-12-13 17:32:37 +0000

    CW7, needed for strrchr.

--- zoolib/ZLog.cpp
+++ zoolib/ZLog.cpp
@@ -18,6 +18,7 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
+#include "zoolib/ZCompat_string.h"
 #include "zoolib/ZLog.h"
 #include "zoolib/ZString.h"
 #include "zoolib/ZThread.h"
