d30244dfbe995b326966d76c44f31796a877d7fc
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2010-12-27 04:06:25 +0000

    Fix copyright date.

--- zoolib/ZYad_MapAsSeq.cpp
+++ zoolib/ZYad_MapAsSeq.cpp
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------------------------------
-Copyright (c) 2009 Andrew Green
+Copyright (c) 2010 Andrew Green
 http://www.zoolib.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
