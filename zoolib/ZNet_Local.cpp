7aaf51a62523d1601a84c6f563d4333908f4f564
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-01-10 12:36:34 +0000

    Remove unnecessary include.

--- zoolib/ZNet_Local.cpp
+++ zoolib/ZNet_Local.cpp
@@ -20,7 +20,6 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ZNet_Local.h"
 #include "zoolib/ZFunctionChain.h"
-#include "zoolib/ZString.h"
 
 namespace ZooLib {
 
