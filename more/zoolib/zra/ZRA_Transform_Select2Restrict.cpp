9f7896f47f783d2b853d86583127b46c0b128ae3
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-01-27 19:52:47 +0000

    Turns a select into a sequence of restricts.

---
+++ more/zoolib/zra/ZRA_Transform_Select2Restrict.cpp
@@ -0,0 +1,58 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2011 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zoolib/ZUtil_STL_map.h"
+#include "zoolib/ZUtil_Expr_Bool_CNF.h"
+#include "zoolib/ZExpr_Bool_ValPred.h"
+#include "zoolib/zra/ZRA_Expr_Rel_Restrict.h"
+#include "zoolib/zra/ZRA_Transform_Select2Restrict.h"
+
+using std::set;
+
+namespace ZooLib {
+namespace ZRA {
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * Transform_Select2Restrict
+
+void Transform_Select2Restrict::Visit_Expr_Rel_Select(const ZRef<Expr_Rel_Select>& iExpr)
+	{
+	using Util_Expr_Bool::CNF;
+	using Util_Expr_Bool::Disjunction;
+
+	ZRef<Expr_Rel> theRel = iExpr->GetOp0();
+	const CNF theCNF = Util_Expr_Bool::sAsCNF(iExpr->GetExpr_Bool());
+	for (CNF::const_iterator i = theCNF.begin(); i != theCNF.end(); ++i)
+		{
+		set<ZValPred> theValPreds;
+		for (Disjunction::const_iterator j = i->begin(); j != i->end(); ++j)
+			{
+			if (ZRef<ZExpr_Bool_ValPred> theVP = j->Get().DynamicCast<ZExpr_Bool_ValPred>())
+				theValPreds.insert(theVP->GetValPred());
+			}
+		theRel = sRestrict(theValPreds, theRel);
+		}
+
+	this->pSetResult(theRel);
+	}
+
+} // namespace ZRA
+} // namespace ZooLib
