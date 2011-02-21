e3cfd12e73c100c33812c68815a4fc76d60e37ba
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-02-21 17:44:16 +0000

    Tentative implementation of something that will let us refcount anything.

---
+++ zoolib/ZCountedWrap.h
@@ -0,0 +1,62 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2011 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZCountedWrap__
+#define __ZCountedWrap__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZCountedWithoutFinalize.h"
+
+namespace ZooLib {
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZCountedWrap
+
+template <class T>
+class ZCountedWrap
+:	public T
+,	public ZCountedWithoutFinalize
+	{
+public:
+	ZCountedWrap()
+		{}
+
+	ZCountedWrap(const T& iOther)
+	:	T(iOther)
+		{}
+
+	ZRef<ZCountedWrap> Clone()
+		{ return new ZCountedWrap(*this); }
+	
+	ZRef<ZCountedWrap> Fresh()
+		{
+		if (this->IsShared())
+			return this->Clone();
+		return this;
+		}
+
+	T& GetWrap()
+		{ return *this; }
+	};
+
+} // namespace ZooLib
+
+#endif // __ZCountedWrap__
