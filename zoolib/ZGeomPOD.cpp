4a8ee2adaf8d4d399d7f1ff2cbb73c1767fa97e7
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-05-28 16:59:46 +0000

    Needs ZCompare_Integer.h in some cases.

--- zoolib/ZGeomPOD.cpp
+++ zoolib/ZGeomPOD.cpp
@@ -19,6 +19,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
 #include "zoolib/ZGeomPOD.h"
+#include "zoolib/ZCompare_Integer.h"
 
 namespace ZooLib {
 
