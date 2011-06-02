d896c24caac05f1c0af48cf8614696cec2c1a851
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-06-02 22:43:08 +0000

    Remove warning.

--- zoolib/ZFile_POSIX.cpp
+++ zoolib/ZFile_POSIX.cpp
@@ -538,6 +538,9 @@ static void spSplit(
 		}
 	}
 
+#if ZCONFIG(Compiler,GCC)
+__attribute__((unused))
+#endif
 static void spSplit(char iSep, bool iIncludeEmpties, const string& iPath, vector<string>& oComps)
 	{
 	if (size_t length = iPath.length())
