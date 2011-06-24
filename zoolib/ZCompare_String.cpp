db6d7df5cf650546d27ff5eb2ce2b2e248827867
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-06-24 14:44:33 +0000

    Need ZStdInt.h for size_t in some cases.

--- zoolib/ZCompare_String.cpp
+++ zoolib/ZCompare_String.cpp
@@ -19,8 +19,9 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
 #include "zoolib/ZCompare.h"
-#include "zoolib/ZMemory.h"
 #include "zoolib/ZCompare_String.h"
+#include "zoolib/ZMemory.h"
+#include "zoolib/ZStdInt.h"
 
 namespace ZooLib {
 
