dab87f910a0cede22b4f12a4f8697deae125b39f
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-07-04 22:56:19 +0000

    A thread-specific type-delineated shared variable.

---
+++ zoolib/ZThreadValue.h
@@ -0,0 +1,71 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2011 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,Publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZThreadValue__
+#define __ZThreadValue__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZThread.h"
+
+namespace ZooLib {
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZThreadValue
+
+template <class T>
+class ZThreadValue
+	{
+public:
+	ZThreadValue(const T& iValue)
+	:	fValue(iValue)
+	,	fPrior((ZThreadValue*)(ZTSS::sGet(spKey())))
+		{ ZTSS::sSet(spKey(), this); }
+
+	~ZThreadValue()
+		{ ZTSS::sSet(spKey(), fPrior); }
+
+	T& GetMutable()
+		{ return fValue; }
+
+	const T& Get() const
+		{ return fValue; }
+	
+	static T& sGetMutable()
+		{ return ((ZThreadValue*)(ZTSS::sGet(spKey())))->GetMutable(); }
+
+	static const T& sGet()
+		{ return ((const ZThreadValue*)(ZTSS::sGet(spKey())))->Get(); }
+
+private:
+	static ZTSS::Key& spKey()
+		{
+		// This method is a sneaky way to have static storage for a template class.
+		static ZTSS::Key spKey = ZTSS::sCreate();
+		return spKey;
+		}
+
+	T fValue;
+	ZThreadValue* fPrior;
+	};
+
+} // namespace ZooLib
+
+#endif // __ZThreadValue__
