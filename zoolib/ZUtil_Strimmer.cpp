1dc31484b3303d224d126bcc9c8940a9c2b6cb93
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-07-25 00:40:37 +0000

    Very common to take a streamer and want a strimmer.

---
+++ zoolib/ZUtil_Strimmer.cpp
@@ -0,0 +1,49 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2010 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zoolib/ZStrim_Stream.h"
+#include "zoolib/ZStrimmer_Streamer.h"
+#include "zoolib/ZStrimU_Unreader.h"
+#include "zoolib/ZUtil_Strimmer.h"
+
+namespace ZooLib {
+namespace ZUtil_Strimmer {
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZUtil_Strimmer
+
+ZRef<ZStrimmerU> sStrimmerU(ZRef<ZStreamerR> iStreamerR)
+	{
+	if (iStreamerR)
+		{
+		ZRef<ZStrimmerR> theStrimmerR_StreamUTF8 =
+			new ZStrimmerR_Streamer_T<ZStrimR_StreamUTF8>(iStreamerR);
+
+		ZRef<ZStrimmerU> theStrimmerU_Unreader =
+			new ZStrimmerU_FT<ZStrimU_Unreader>(theStrimmerR_StreamUTF8);
+
+		return theStrimmerU_Unreader;
+		}
+	return null;
+	}
+
+} // namespace ZUtil_Strimmer
+} // namespace ZooLib
