a41052b04a984f3274f448cf39611aaa0931a082
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-08-02 18:06:42 +0000

    Although ZCaller looks like a reasonable candidate for just being a typedef of ZCallable I found it to be somewhat confusing to do so. It's now its own class, with a single Queue method, which returns true if the callable was or will be called, false otherwise.

---
+++ zoolib/ZCaller.h
@@ -0,0 +1,55 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2011 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,Publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#ifndef __ZCaller__
+#define __ZCaller__ 1
+#include "zconfig.h"
+
+#include "zoolib/ZCallable.h"
+
+namespace ZooLib {
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZCaller
+
+class ZCaller
+:	public ZCounted
+	{
+public:
+	virtual bool Queue(ZRef<ZCallable_Void> iCallable) = 0;
+	};
+
+// =================================================================================================
+#pragma mark -
+#pragma mark * ZCaller_Null
+
+class ZCaller_Null
+:	public ZCaller
+	{
+public:
+// From ZCaller
+	virtual bool Queue(ZRef<ZCallable_Void> iCallable)
+		{ return sQCall(iCallable); }
+	};
+
+} // namespace ZooLib
+
+#endif // __ZCaller__
