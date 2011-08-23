43f74998f39b834d9c6e69679cb22e074bb77c28
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-08-23 12:04:33 +0000

    We make reference to ZRef in ZCountedVal.

--- zoolib/ZCountedVal.h
+++ zoolib/ZCountedVal.h
@@ -23,6 +23,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "zconfig.h"
 
 #include "zoolib/ZCountedWithoutFinalize.h"
+#include "zoolib/ZRef.h"
 #include "zoolib/ZTagVal.h"
 
 namespace ZooLib {
