8bb8f760c4ddc61ac2a326adb34780ed7be4ad81
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-10-16 04:26:02 +0000

    Add operator_bool for ZVal_yad.

--- zoolib/ZVal_Yad.cpp
+++ zoolib/ZVal_Yad.cpp
@@ -30,6 +30,9 @@ namespace ZooLib {
 ZAny ZVal_Yad::AsAny() const
 	{ return this->pAsAny(); }
 
+ZVal_Yad::operator operator_bool() const
+	{ return operator_bool_gen::translate(fYad); }
+
 ZVal_Yad::ZVal_Yad()
 	{}
 
--- zoolib/ZVal_Yad.h
+++ zoolib/ZVal_Yad.h
@@ -42,6 +42,7 @@ class ZVal_Yad
 	{
 public:
 	ZAny AsAny() const;
+	ZMACRO_operator_bool(ZVal_Yad, operator_bool) const;
 
 	ZVal_Yad();
 	ZVal_Yad(const ZVal_Yad& iOther);
