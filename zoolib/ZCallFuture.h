68643c684e1810d1bbfbf65b07fb8b55f702f97b
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-10-18 20:19:20 +0000

    We need to see a definition for ZCaller.

--- zoolib/ZCallFuture.h
+++ zoolib/ZCallFuture.h
@@ -22,6 +22,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define __ZCallFuture__ 1
 #include "zconfig.h"
 
+#include "zoolib/ZCaller.h"
 #include "zoolib/ZCallable.h"
 #include "zoolib/ZCallable_Bind.h"
 #include "zoolib/ZCallable_Function.h"
