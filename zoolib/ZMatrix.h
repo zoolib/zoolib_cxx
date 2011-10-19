6f6177ce1b4ee6e082106aca2c6052142a9af1a4
agreen <agreen@b95690a7-434c-0410-acd0-f892efc248cd>
2011-10-19 18:09:36 +0000

    We use ZAssert and so need ZDebug.h.

--- zoolib/ZMatrix.h
+++ zoolib/ZMatrix.h
@@ -22,6 +22,8 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define __ZMatrix__ 1
 #include "zconfig.h"
 
+#include "zoolib/ZDebug.h"
+
 namespace ZooLib {
 
 // For compatibility with OpenGL we store in column-major order.
