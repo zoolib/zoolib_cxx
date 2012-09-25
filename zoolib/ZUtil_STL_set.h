bb0c9db3ec2f93497685e6ffe44220a66927c0f0
Andrew Green <ag@em.net>
2012-09-25 15:10:03 -0500

    Set uses ZQ.

--- zoolib/ZUtil_STL_set.h
+++ zoolib/ZUtil_STL_set.h
@@ -23,6 +23,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "zconfig.h"
 
 #include "zoolib/ZDebug.h"
+#include "zoolib/ZQ.h"
 
 #include <iterator> // For inserter
 #include <set>
