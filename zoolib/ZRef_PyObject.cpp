5a46fbb6340d2d7a80b9eef3a7f156e4e0f41b5b
Andrew Green <ag@em.net>
2013-01-31 10:36:51 -0600

    Refcount really needs to be manipulated under the protection of the GIL.

--- zoolib/ZRef_PyObject.cpp
+++ zoolib/ZRef_PyObject.cpp
@@ -25,13 +25,21 @@ namespace ZooLib {
 template <> void sRetain_T(PyObject*& ioObject)
 	{
 	if (ioObject)
+		{
+		PyGILState_STATE gstate = PyGILState_Ensure();
 		Py_INCREF(ioObject);
+		PyGILState_Release(gstate);
+		}
 	}
 
 template <> void sRelease_T(PyObject* iObject)
 	{
 	if (iObject)
+		{
+		PyGILState_STATE gstate = PyGILState_Ensure();
 		Py_DECREF(iObject);
+		PyGILState_Release(gstate);
+		}
 	}
 
 } // namespace ZooLib
