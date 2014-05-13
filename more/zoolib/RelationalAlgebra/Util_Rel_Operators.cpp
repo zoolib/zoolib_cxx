dc615e75d396040ac908b7cd8dac99c689ea4bdd
Andrew Green <ag@em.net>
2014-05-13 18:10:49 -0500

    Rename, to make clear that these are operators for rels, rather than something to do with relops.

--- more/zoolib/RelationalAlgebra/Util_Rel_Operators.cpp
+++ more/zoolib/RelationalAlgebra/Util_Rel_Operators.cpp
@@ -18,7 +18,7 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
-#include "zoolib/RelationalAlgebra/Util_RelOperators.h"
+#include "zoolib/RelationalAlgebra/Util_Rel_Operators.h"
 
 namespace ZooLib {
 namespace RelationalAlgebra {
