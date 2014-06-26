00de5335f58110e54f47f46b708d97f1fa3e58d5
Andrew Green <ag@em.net>
2014-06-26 15:39:47 -0500

    A slightly different formulation that makes more sense.

--- zoolib/ZAccumulator_T.h
+++ zoolib/ZAccumulator_T.h
@@ -46,14 +46,16 @@ namespace ZooLib {
 template <typename T, typename C, typename S>
 class ZAccumulator_T
 	{
-	std::bitset;
-	static inline uint32 sHammingWeight(uint32 w)
+	static inline uint32 sHammingWeight(uint32 n)
 		{
-		uint32 res = (w & 0x55555555) + ((w >> 1) & 0x55555555);
-		res = (res & 0x33333333) + ((res >> 2) & 0x33333333);
-		res = (res & 0x0F0F0F0F) + ((res >> 4) & 0x0F0F0F0F);
-		res = (res & 0x00FF00FF) + ((res >> 8) & 0x00FF00FF);
-		return (res & 0x0000FFFF) + ((res >> 16) & 0x0000FFFF);
+		// See <https://stackoverflow.com/revisions/9830282/2>
+		// for the nicest explanation I've seen of this.
+		n = (n & 0x55555555) + ((n & 0xAAAAAAAA) >> 1);
+		n = (n & 0x33333333) + ((n & 0xCCCCCCCC) >> 2);
+		n = (n & 0x0F0F0F0F) + ((n & 0xF0F0F0F0) >> 4);
+		n = (n & 0x00FF00FF) + ((n & 0xFF00FF00) >> 8);
+		n = (n & 0x0000FFFF) + ((n & 0xFFFF0000) >> 16);
+		return n;
 		}
 
 public:
