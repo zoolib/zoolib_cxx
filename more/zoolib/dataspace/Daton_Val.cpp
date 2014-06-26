70d503b75a90f58537b80dfa49ec19e85f17107e
Andrew Green <ag@em.net>
2014-06-26 15:37:59 -0500

    Moved out into its own file.

---
+++ more/zoolib/dataspace/Daton_Val.cpp
@@ -0,0 +1,75 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2014 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zooLib/dataspace/Daton_Val.h"
+
+#include "zoolib/ZStream_Data_T.h"
+#include "zoolib/ZStrim_Stream.h"
+#include "zoolib/ZStrimU_StreamUTF8Buffered.h"
+#include "zoolib/ZStrimmer_Streamer.h"
+#include "zoolib/ZYad_Any.h"
+#include "zoolib/ZYad_ZooLibStrim.h"
+
+namespace ZooLib {
+namespace Dataspace {
+
+// =================================================================================================
+// MARK: - spAsVal (anonymous)
+
+namespace { // anonymous
+
+ZVal_Any spAsVal(const ZData_Any& iData)
+	{
+	try
+		{
+		ZRef<ZStreamerR> theStreamerR =
+			new ZStreamerRPos_T<ZStreamRPos_Data_T<ZData_Any> >(iData);
+
+		ZRef<ZStrimmerU> theStrimmerU =
+			new ZStrimmerU_Streamer_T<ZStrimU_StreamUTF8Buffered>(1024, theStreamerR);
+
+		ZRef<ZYadR> theYadR = ZYad_ZooLibStrim::sYadR(theStrimmerU);
+
+		return sFromYadR(ZVal_Any(), theYadR);
+		}
+	catch (...)
+		{
+		return ZVal_Any();
+		}
+	}
+
+} // anonymous namespace
+
+// =================================================================================================
+// MARK: - Daton/Val conversion.
+
+ZVal_Any sAsVal(const ZDatonSet::Daton& iDaton)
+	{ return spAsVal(iDaton.GetData()); }
+
+ZDatonSet::Daton sAsDaton(const ZVal_Any& iVal)
+	{
+	ZData_Any theData;
+	ZYad_ZooLibStrim::sToStrim(sYadR(iVal),
+		ZStrimW_StreamUTF8(sStreamRWPos_Data_T(theData)));
+	return theData;
+	}
+
+} // namespace Dataspace
+} // namespace ZooLib
