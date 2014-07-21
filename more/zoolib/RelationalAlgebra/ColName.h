fcc71f49f05beef48b5a0c246513e23488cfc2dc
Andrew Green <ag@em.net>
2014-07-20 19:37:56 -0500

    We're based off string8, don't need all of UnicodeString.

--- more/zoolib/RelationalAlgebra/ColName.h
+++ more/zoolib/RelationalAlgebra/ColName.h
@@ -22,7 +22,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define __ZooLib_RelationalAlgebra_ColName_h__ 1
 #include "zconfig.h"
 
-#include "zoolib/ZUnicodeString.h"
+#include "zoolib/ZUnicodeString8.h"
 
 namespace ZooLib {
 namespace RelationalAlgebra {
