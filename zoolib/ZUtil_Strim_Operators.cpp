0f9cd24867f5b983bc7e48c6a0c2112a4d85bf1a
Andrew Green <ag@em.net>
2014-08-29 15:06:12 -0500

    'char' is a what '' produces. 'signed char' and 'unsigned char' are integers of the same size as a char, but distinct and distinguishable in type from it.

--- zoolib/ZUtil_Strim_Operators.cpp
+++ zoolib/ZUtil_Strim_Operators.cpp
@@ -60,13 +60,13 @@ const ZStrimW& operator<<(const ZStrimW& w, bool iVal)
 	{ return w.Write(iVal ? "true" : "false"); }
 
 const ZStrimW& operator<<(const ZStrimW& w, char iVal)
-	{ return w.Writef("%d", int(iVal)); }
+	{ return w.Writef("%c", iVal); }
 
 const ZStrimW& operator<<(const ZStrimW& w, unsigned char iVal)
 	{ return w.Writef("%u", (unsigned int)iVal); }
 
 const ZStrimW& operator<<(const ZStrimW& w, signed char iVal)
-	{ return w.Writef("%c", iVal); }
+	{ return w.Writef("%d", int(iVal)); }
 
 const ZStrimW& operator<<(const ZStrimW& w, __wchar_t iVal)
 	{ return w.Writef("%lc", iVal); }
