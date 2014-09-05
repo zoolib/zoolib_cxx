e4fc3ba4b95fc9145c404a9af824ab47f001dfd5
Andrew Green <ag@em.net>
2014-09-05 15:23:10 -0500

    Some NativeUTF32, NativeUTF16, NativeUTF8 chan implementations.

--- core_c/zoolib/ChanR_UTF_More.cpp
+++ core_c/zoolib/ChanR_UTF_More.cpp
@@ -20,21 +20,91 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ChanR_UTF_More.h"
 
+#include "zoolib/ZMemory.h"
+#include "zoolib/ZUnicode.h"
+
 namespace ZooLib {
 
+using std::max;
+using std::min;
+
+static const size_t kBufSize = sStackBufferSize;
+#define kDebug_Strim 2
+
+#if 0
 // =================================================================================================
 // MARK: - ChanR_UTF_Native32
 
 void ChanR_UTF_Native32::ReadUTF16(UTF16* oDest,
 	 size_t iCountCU, size_t* oCountCU, size_t iCountCP, size_t* oCountCP)
 	{
-	ZUnimplemented();
+	UTF32 utf32Buffer[kBufSize];
+	UTF16* localDest = oDest;
+	size_t localCountCP = iCountCP;
+	// If we've got space for two code units we are assured of
+	// being able to read at least one code point.
+	while (iCountCU >= 2 && localCountCP)
+		{
+		const size_t utf32Read = this->Read(utf32Buffer, min(kBufSize, min(localCountCP, iCountCU)));
+		if (utf32Read == 0)
+			break;
+
+		size_t utf32Consumed;
+		size_t utf16Generated;
+		size_t cpGenerated;
+		ZUnicode::sUTF32ToUTF16(
+			utf32Buffer, utf32Read,
+			&utf32Consumed, nullptr,
+			localDest, iCountCU,
+			&utf16Generated,
+			localCountCP, &cpGenerated);
+
+		ZAssertStop(kDebug_Strim, utf32Consumed == utf32Read);
+
+		localCountCP -= cpGenerated;
+		iCountCU -= utf16Generated;
+		localDest += utf16Generated;
+		}
+	if (oCountCP)
+		*oCountCP = iCountCP - localCountCP;
+	if (oCountCU)
+		*oCountCU = localDest - oDest;
 	}
 
 void ChanR_UTF_Native32::ReadUTF8(UTF8* oDest,
 	size_t iCountCU, size_t* oCountCU, size_t iCountCP, size_t* oCountCP)
 	{
-	ZUnimplemented();
+	UTF32 utf32Buffer[kBufSize];
+	UTF8* localDest = oDest;
+	size_t localCountCP = iCountCP;
+	// If we've got space for six code units we are assured of
+	// being able to read at least one code point.
+	while (iCountCU >= 6 && localCountCP)
+		{
+		const size_t utf32Read = this->Read(utf32Buffer, min(kBufSize, min(localCountCP, iCountCU)));
+		if (utf32Read == 0)
+			break;
+
+		size_t utf32Consumed;
+		size_t utf8Generated;
+		size_t cpGenerated;
+		ZUnicode::sUTF32ToUTF8(
+			utf32Buffer, utf32Read,
+			&utf32Consumed, nullptr,
+			localDest, iCountCU,
+			&utf8Generated,
+			localCountCP, &cpGenerated);
+
+		ZAssertStop(kDebug_Strim, utf32Consumed == utf32Read);
+
+		localCountCP -= cpGenerated;
+		iCountCU -= utf8Generated;
+		localDest += utf8Generated;
+		}
+	if (oCountCP)
+		*oCountCP = iCountCP - localCountCP;
+	if (oCountCU)
+		*oCountCU = localDest - oDest;
 	}
 
 // =================================================================================================
@@ -43,26 +113,158 @@ void ChanR_UTF_Native32::ReadUTF8(UTF8* oDest,
 size_t ChanR_UTF_Native16::Read(UTF32* oDest, size_t iCountCU)
 	{
 	ZUnimplemented();
+
+	// This is a problem one -- we may have to nibble-Read and gradually fill the buffer.
+	#if 0
+	UTF16 utf16Buffer[kBufSize];
+	UTF32* localDest = oDest;
+	while (iCountCU)
+		{
+		const size_t utf16Read = this->Read(utf16Buffer, min(kBufSize, max<size_t>(2, iCountCU)));
+		if (utf16Read == 0)
+			break;
+
+		size_t utf16Consumed;
+		size_t utf32Generated;
+		ZUnicode::sUTF16ToUTF32(
+			utf16Buffer, utf16Read,
+			&utf16Consumed, nullptr,
+			localDest, iCountCU,
+			&utf32Generated);
+
+		ZAssertStop(kDebug_Strim, utf16Consumed == utf16Read);
+
+		iCountCU -= utf32Generated;
+		localDest += utf32Generated;
+		}
+	return localDest - oDest;
+	#endif
 	}
 
 void ChanR_UTF_Native16::ReadUTF8(UTF8* oDest,
 	size_t iCountCU, size_t* oCountCU, size_t iCountCP, size_t* oCountCP)
 	{
 	ZUnimplemented();
+
+	UTF16 utf16Buffer[kBufSize];
+	UTF8* localDest = oDest;
+	size_t localCountCP = iCountCP;
+	// If we've got space for six code units we are assured of
+	// being able to read at least one code point.
+	while (iCountCU >= 6 && localCountCP)
+		{
+		const size_t utf16Read = this->Read(utf16Buffer, min(kBufSize, min(localCountCP, iCountCU)));
+		if (utf16Read == 0)
+			break;
+
+		size_t utf16Consumed;
+		size_t utf8Generated;
+		size_t cpGenerated;
+		ZUnicode::sUTF16ToUTF8(
+			utf16Buffer, utf16Read,
+			&utf16Consumed, nullptr,
+			localDest, iCountCU,
+			&utf8Generated,
+			localCountCP, &cpGenerated);
+
+		ZAssertStop(kDebug_Strim, utf16Consumed == utf16Read);
+
+		localCountCP -= cpGenerated;
+		iCountCU -= utf8Generated;
+		localDest += utf8Generated;
+		}
+	if (oCountCP)
+		*oCountCP = iCountCP - localCountCP;
+	if (oCountCU)
+		*oCountCU = localDest - oDest;
 	}
+#endif
 
 // =================================================================================================
 // MARK: - ChanR_UTF_Native8
 
 size_t ChanR_UTF_Native8::Read(UTF32* oDest, size_t iCountCU)
 	{
-	ZUnimplemented();
+	UTF8 utf8Buffer[kBufSize];
+	size_t utf8Buffered = 0;
+
+	UTF32* localDest = oDest;
+
+	while (iCountCU)
+		{
+		const size_t utf8Read = this->Read(utf8Buffer, min(kBufSize - utf8Buffered, iCountCU));
+		if (utf8Read == 0)
+			break;
+
+		utf8Buffered += utf8Read;
+
+		size_t utf8Consumed;
+		size_t utf32Generated;
+		ZUnicode::sUTF8ToUTF32(
+			utf8Buffer, utf8Read,
+			&utf8Consumed, nullptr,
+			localDest, iCountCU,
+			&utf32Generated);
+
+		iCountCU -= utf32Generated;
+		localDest += utf32Generated;
+
+		if (utf8Consumed && utf8Buffered > utf8Consumed)
+			{
+			sMemMove(utf8Buffer, utf8Buffer + utf8Consumed, utf8Buffered - utf8Consumed);
+			utf8Buffered -= utf8Consumed;
+			}
+		}
+	return localDest - oDest;
 	}
 
 void ChanR_UTF_Native8::ReadUTF16(UTF16* oDest,
 	 size_t iCountCU, size_t* oCountCU, size_t iCountCP, size_t* oCountCP)
 	{
-	ZUnimplemented();
+	UTF8 utf8Buffer[kBufSize];
+	size_t utf8Buffered = 0;
+
+	UTF16* localDest = oDest;
+	size_t localCountCP = iCountCP;
+
+	// If we've got space for two code units we are assured of
+	// being able to read at least one code point.
+	while (iCountCU >= 2 && localCountCP)
+		{
+		// Top up the buffer as much as we can.
+		const size_t utf8Read = this->Read(utf8Buffer + utf8Buffered,
+			min(kBufSize - utf8Buffered, min(localCountCP, iCountCU)));
+
+		if (utf8Read == 0)
+			break;
+
+		utf8Buffered += utf8Read;
+
+		size_t utf8Consumed;
+		size_t utf16Generated;
+		size_t cpGenerated;
+		ZUnicode::sUTF8ToUTF16(
+			utf8Buffer, utf8Buffered,
+			&utf8Consumed, nullptr,
+			localDest, iCountCU,
+			&utf16Generated,
+			localCountCP, &cpGenerated);
+
+		localCountCP -= cpGenerated;
+		iCountCU -= utf16Generated;
+		localDest += utf16Generated;
+
+		if (utf8Consumed && utf8Buffered > utf8Consumed)
+			{
+			sMemMove(utf8Buffer, utf8Buffer + utf8Consumed, utf8Buffered - utf8Consumed);
+			utf8Buffered -= utf8Consumed;
+			}
+		}
+
+	if (oCountCP)
+		*oCountCP = iCountCP - localCountCP;
+	if (oCountCU)
+		*oCountCU = localDest - oDest;
 	}
 
 } // namespace ZooLib
--- core_c/zoolib/ChanW_UTF_More.cpp
+++ core_c/zoolib/ChanW_UTF_More.cpp
@@ -20,46 +20,241 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/ChanW_UTF_More.h"
 #include "zoolib/ZDebug.h"
+#include "zoolib/Zunicode.h"
 
 namespace ZooLib {
+
+static const size_t kBufSize = sStackBufferSize;
+#define kDebug_Strim 2
 
 // =================================================================================================
 // MARK: - ChanW_UTF_Native32
 
 size_t ChanW_UTF_Native32::Write(const UTF16* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF16* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF32 buffer[kBufSize];
+		size_t utf16Consumed;
+		size_t utf32Generated;
+		if (not ZUnicode::sUTF16ToUTF32(
+			localSource, iCountCU,
+			&utf16Consumed, nullptr,
+			buffer, kBufSize,
+			&utf32Generated))
+			{
+			// localSource[0] to localSource[iCountCU] ends with an incomplete UTF16 sequence.
+			if (utf16Consumed == 0)
+				{
+				// In fact there was no valid UTF16 in localSource at all, so we'll just bail.
+				break;
+				}
+			}
+
+		size_t utf32Consumed = this->Write(buffer, utf32Generated);
+		if (utf32Consumed < utf32Generated)
+			{
+			// It's a truncated write. We need to convert from utf32Consumed back into
+			// the number of utf16 code units.
+			localSource += ZUnicode::sCPToCU(localSource, utf32Consumed);
+			break;
+			}
+
+		localSource += utf16Consumed;
+		iCountCU -= utf16Consumed;
+		}
+	return localSource - iSource;
 	}
 
 size_t ChanW_UTF_Native32::Write(const UTF8* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF8* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF32 buffer[kBufSize];
+		size_t utf8Consumed;
+		size_t utf32Generated;
+		if (not ZUnicode::sUTF8ToUTF32(
+			localSource, iCountCU,
+			&utf8Consumed, nullptr,
+			buffer, kBufSize,
+			&utf32Generated))
+			{
+			// localSource[0] to localSource[iCountCU] ends with an incomplete UTF8 sequence.
+			if (utf8Consumed == 0)
+				{
+				// In fact there was no valid UTF8 in localSource at all, so it's time to bail.
+				break;
+				}
+			}
+
+		size_t utf32Consumed = this->Write(buffer, utf32Generated);
+		if (utf32Consumed < utf32Generated)
+			{
+			// It's a truncated write. We need to convert from utf32Consumed back into
+			// the number of utf8 code units.
+			localSource += ZUnicode::sCPToCU(localSource, utf32Consumed);
+			break;
+			}
+
+		localSource += utf8Consumed;
+		iCountCU -= utf8Consumed;
+		}
+	return localSource - iSource;
 	}
 
 // =================================================================================================
 // MARK: - ChanW_UTF_Native16
 
-size_t ChanW_UTF_Native16::Write(const UTF32* iSource, size_t iCount)
+size_t ChanW_UTF_Native16::Write(const UTF32* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF32* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF16 buffer[kBufSize];
+
+		size_t utf32Consumed;
+		size_t utf16Generated;
+		ZUnicode::sUTF32ToUTF16(
+			localSource, iCountCU,
+			&utf32Consumed, nullptr,
+			buffer, kBufSize,
+			&utf16Generated,
+			kBufSize, nullptr);
+
+		ZAssertStop(kDebug_Strim, utf32Consumed <= iCountCU);
+		ZAssertStop(kDebug_Strim, utf16Generated <= kBufSize);
+
+		size_t utf16Consumed = this->Write(buffer, utf16Generated);
+		if (utf16Consumed < utf16Generated)
+			{
+			// It's a truncated write. We need to convert from utf16Consumed back into
+			// the number of code points, which is also the number of UTF32 code units.
+			localSource += ZUnicode::sCUToCP(buffer, utf16Consumed);
+			break;
+			}
+
+		localSource += utf32Consumed;
+		iCountCU -= utf32Consumed;
+		}
+	return localSource - iSource;
 	}
 
 size_t ChanW_UTF_Native16::Write(const UTF8* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF8* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF16 buffer[kBufSize];
+		size_t utf8Consumed;
+		size_t utf16Generated;
+		if (not ZUnicode::sUTF8ToUTF16(
+			localSource, iCountCU,
+			&utf8Consumed, nullptr,
+			buffer, kBufSize,
+			&utf16Generated,
+			iCountCU, nullptr))
+			{
+			// localSource[0] to localSource[iCountCU] ends with an incomplete UTF8 sequence.
+			if (utf8Consumed == 0)
+				{
+				// In fact there was no valid UTF8 in localSource at all, so it's time to bail.
+				break;
+				}
+			}
+
+		size_t utf16Consumed = this->Write(buffer, utf16Generated);
+		if (utf16Consumed < utf16Generated)
+			{
+			// It's a truncated write. We need to convert from utf16Consumed back into
+			// the number of utf8 code units.
+			const size_t codePoints = ZUnicode::sCUToCP(buffer, utf16Consumed);
+			localSource += ZUnicode::sCPToCU(localSource, codePoints);
+			break;
+			}
+
+		localSource += utf8Consumed;
+		iCountCU -= utf8Consumed;
+		}
+	return localSource - iSource;
 	}
 
 // =================================================================================================
 // MARK: - ChanW_UTF_Native8
 
-size_t ChanW_UTF_Native8::Write(const UTF32* iSource, size_t iCount)
+size_t ChanW_UTF_Native8::Write(const UTF32* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF32* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF8 buffer[kBufSize];
+
+		size_t utf32Consumed;
+		size_t utf8Generated;
+		ZUnicode::sUTF32ToUTF8(
+			localSource, iCountCU,
+			&utf32Consumed, nullptr,
+			buffer, kBufSize,
+			&utf8Generated,
+			kBufSize, nullptr);
+
+		ZAssertStop(kDebug_Strim, utf32Consumed <= iCountCU);
+		ZAssertStop(kDebug_Strim, utf8Generated <= kBufSize);
+
+		size_t utf8Consumed = this->Write(buffer, utf8Generated);
+		if (utf8Consumed < utf8Generated)
+			{
+			// It's a truncated write. We need to convert from utf8Consumed back into
+			// the number UTF32 code units..
+			const size_t codePoints = ZUnicode::sCUToCP(buffer, utf8Consumed);
+			localSource += ZUnicode::sCPToCU(localSource, codePoints);
+			break;
+			}
+
+		localSource += utf32Consumed;
+		iCountCU -= utf32Consumed;
+		}
+	return localSource - iSource;
 	}
 
 size_t ChanW_UTF_Native8::Write(const UTF16* iSource, size_t iCountCU)
 	{
-	ZUnimplemented();
+	const UTF16* localSource = iSource;
+	while (iCountCU)
+		{
+		UTF8 buffer[kBufSize];
+		size_t utf16Consumed;
+		size_t utf8Generated;
+		if (not ZUnicode::sUTF16ToUTF8(
+			localSource, iCountCU,
+			&utf16Consumed, nullptr,
+			buffer, kBufSize,
+			&utf8Generated,
+			iCountCU, nullptr))
+			{
+			// localSource[0] to localSource[iCountCU] ends with an incomplete UTF16 sequence.
+			if (utf16Consumed == 0)
+				{
+				// In fact there was no valid UTF16 in localSource at all, so it's time to bail.
+				break;
+				}
+			}
+
+		size_t utf8Consumed = this->Write(buffer, utf8Generated);
+		if (utf8Consumed < utf8Generated)
+			{
+			// It's a truncated write. We need to convert from utf8Consumed back into
+			// the number of utf16 code units.
+			const size_t codePoints = ZUnicode::sCUToCP(buffer, utf8Consumed);
+			localSource += ZUnicode::sCPToCU(localSource, codePoints);
+			break;
+			}
+
+		localSource += utf16Consumed;
+		iCountCU -= utf16Consumed;
+		}
+	return localSource - iSource;
 	}
 
 } // namespace ZooLib
