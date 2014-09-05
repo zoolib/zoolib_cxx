0aedea19e5efc56f302d50850fd8c5da5d92aade
Andrew Green <ag@em.net>
2014-09-05 10:26:02 -0500

    Comment.

--- core/zoolib/ZUtil_Relops.h
+++ core/zoolib/ZUtil_Relops.h
@@ -22,7 +22,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define __ZUtil_Relops_h__
 #include "zconfig.h"
 
-#include "ZTypes.h"
+#include "ZTypes.h" // For EnableIfC
 
 namespace ZooLib {
 
