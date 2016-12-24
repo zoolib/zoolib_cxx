460b2eaf6612f9fcfbf99581cf34c82f2cae5ba4
Andrew Green <ag@em.net>
2016-12-24 16:13:17 -0600

    .

--- Platform/zoolib/UIKit/UITVController_WithSections.mm
+++ Platform/zoolib/UIKit/UITVController_WithSections.mm
@@ -1301,55 +1301,6 @@ static void spInsertSections(UITableView* iTableView,
 	return null;
 	}
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
-    {
-    if ([cell respondsToSelector:@selector(tintColor)]) {
-        CGFloat cornerRadius = 5.f;
-        cell.backgroundColor = UIColor.clearColor;
-        CAShapeLayer *layer = [[CAShapeLayer alloc] init];
-        CGMutablePathRef pathRef = CGPathCreateMutable();
-        CGRect bounds = CGRectInset(cell.bounds, 10, 0);
-        BOOL addLine = NO;
-        if (indexPath.row == 0 && indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) {
-            CGPathAddRoundedRect(pathRef, nil, bounds, cornerRadius, cornerRadius);
-        } else if (indexPath.row == 0) {
-            CGPathMoveToPoint(pathRef, nil, CGRectGetMinX(bounds), CGRectGetMaxY(bounds));
-            CGPathAddArcToPoint(pathRef, nil, CGRectGetMinX(bounds), CGRectGetMinY(bounds), CGRectGetMidX(bounds), CGRectGetMinY(bounds), cornerRadius);
-            CGPathAddArcToPoint(pathRef, nil, CGRectGetMaxX(bounds), CGRectGetMinY(bounds), CGRectGetMaxX(bounds), CGRectGetMidY(bounds), cornerRadius);
-            CGPathAddLineToPoint(pathRef, nil, CGRectGetMaxX(bounds), CGRectGetMaxY(bounds));
-            addLine = YES;
-        } else if (indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) {
-            CGPathMoveToPoint(pathRef, nil, CGRectGetMinX(bounds), CGRectGetMinY(bounds));
-            CGPathAddArcToPoint(pathRef, nil, CGRectGetMinX(bounds), CGRectGetMaxY(bounds), CGRectGetMidX(bounds), CGRectGetMaxY(bounds), cornerRadius);
-            CGPathAddArcToPoint(pathRef, nil, CGRectGetMaxX(bounds), CGRectGetMaxY(bounds), CGRectGetMaxX(bounds), CGRectGetMidY(bounds), cornerRadius);
-            CGPathAddLineToPoint(pathRef, nil, CGRectGetMaxX(bounds), CGRectGetMinY(bounds));
-        } else {
-            CGPathAddRect(pathRef, nil, bounds);
-            addLine = YES;
-        }
-        layer.path = pathRef;
-        CFRelease(pathRef);
-        //set the border color
-        layer.strokeColor = [UIColor lightGrayColor].CGColor;
-        //set the border width
-        layer.lineWidth = 1;
-        layer.fillColor = [UIColor colorWithWhite:1.f alpha:1.0f].CGColor;
-
-        if (addLine) {
-            CALayer *lineLayer = [[CALayer alloc] init];
-            CGFloat lineHeight = (1.f / [UIScreen mainScreen].scale);
-            lineLayer.frame = CGRectMake(CGRectGetMinX(bounds), bounds.size.height-lineHeight, bounds.size.width, lineHeight);
-            lineLayer.backgroundColor = tableView.separatorColor.CGColor;
-            [layer addSublayer:lineLayer];
-        }
-
-        UIView *testView = [[UIView alloc] initWithFrame:bounds];
-        [testView.layer insertSublayer:layer atIndex:0];
-        testView.backgroundColor = UIColor.clearColor;
-        cell.backgroundView = testView;
-    }
-}
-
 @end // implementation UITVHandler_WithSections
 
 // =================================================================================================
