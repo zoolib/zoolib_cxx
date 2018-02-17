a6d9ce44b2f16a5453e0c1e6aa8165a77902e83b
Andrew Green <ag@em.net>
2018-02-17 14:04:53 -0600

    Add TypeListWithoutPrefix, to be able to strip the ZCounted prefix off a channer.

--- Core/zoolib/TypeList.h
+++ Core/zoolib/TypeList.h
@@ -56,6 +56,14 @@ struct TypeListPrefixed
 		typename TL::T4, typename TL::T5, typename TL::T6> Result_t;
 	};
 
+template <class TL>
+struct TypeListWithoutPrefix
+	{
+	typedef TypeList<
+		typename TL::T1, typename TL::T2, typename TL::T3,
+		typename TL::T4, typename TL::T5, typename TL::T6, typename TL::T6> Result_t;
+	};
+
 
 } // namespace ZooLib
 
