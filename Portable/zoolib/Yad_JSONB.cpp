3f23585929efaf64b1e4bdd4336ced8c7facad20
Andrew Green <ag@em.net>
2018-10-29 13:31:36 -0600

    Conform to name change.

--- Portable/zoolib/Yad_JSONB.cpp
+++ Portable/zoolib/Yad_JSONB.cpp
@@ -18,9 +18,9 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
-#include "zoolib/Any_Coerce.h"
 #include "zoolib/ChanR_Bin_More.h"
 #include "zoolib/ChanW_Bin_More.h"
+#include "zoolib/Coerce_Any.h"
 #include "zoolib/Log.h"
 #include "zoolib/NameUniquifier.h" // For sName
 #include "zoolib/Stringf.h"
