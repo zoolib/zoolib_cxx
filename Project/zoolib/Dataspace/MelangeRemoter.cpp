c16bb951548c570c70d5fd821891e1798312453f
Andrew Green <ag@em.net>
2018-11-02 18:37:43 -0600

    Conform to file name change.

--- Project/zoolib/Dataspace/MelangeRemoter.cpp
+++ Project/zoolib/Dataspace/MelangeRemoter.cpp
@@ -18,7 +18,7 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------------------------- */
 
-#include "zoolib/Any_Coerce.h"
+#include "zoolib/Coerce_Any.h"
 #include "zoolib/Callable_Bind.h"
 #include "zoolib/Callable_Function.h"
 #include "zoolib/Callable_PMF.h"
