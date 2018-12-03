2a2c81836490079f5101a07dac2ea5babc1dfb76
Andrew Green <ag@em.net>
2018-12-02 19:24:34 -0600

    Add PullPush_Basic and PullPush_SeparatedValues.

---
+++ Portable/zoolib/PullPush_Basic.cpp
@@ -0,0 +1,85 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2018 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zoolib/PullPush_Basic.h"
+
+#include "zoolib/Chan_UTF_string.h" // For ChanW_UTF_string8
+#include "zoolib/NameUniquifier.h" // For sName
+#include "zoolib/ParseException.h"
+#include "zoolib/Util_Chan.h"
+
+#include "zoolib/ZMACRO_foreach.h"
+
+namespace ZooLib {
+
+using namespace PullPush;
+using std::string;
+
+// =================================================================================================
+#pragma mark - Helpers
+
+namespace { // anonymous
+
+bool spRead_Until(const ChanR_UTF& iChanR, UTF32 iTerminator, string& oString)
+	{
+	oString.clear();
+	return sCopy_Until<UTF32>(iChanR, iTerminator, ChanW_UTF_string8(&oString));
+	}
+
+} // anonymous namespace
+
+// =================================================================================================
+#pragma mark - Pull_Basic_Options
+
+Pull_Basic_Options::Pull_Basic_Options(UTF32 iNameFromValue, UTF32 iEntryFromEntry)
+:	fSeparator_NameFromValue(iNameFromValue)
+,	fSeparator_EntryFromEntry(iEntryFromEntry)
+	{}
+
+// =================================================================================================
+#pragma mark -
+
+bool sPull_Basic_Push(const ChanR_UTF& iChanR, const Pull_Basic_Options& iOptions,
+	const ChanW_Any& iChanW)
+	{
+	for (bool isFirst = true; /*no test*/; isFirst = false)
+		{
+		string theName;
+		if (not spRead_Until(iChanR, iOptions.fSeparator_NameFromValue, theName))
+			{
+			if (isFirst)
+				return false;
+			sPush(kEnd, iChanW);
+			return true;
+			}
+		else if (isFirst)
+			{
+			sPush(kStartMap, iChanW);
+			}
+
+		string theValue;
+		spRead_Until(iChanR, iOptions.fSeparator_EntryFromEntry, theValue);
+
+		sPush(sName(theName), iChanW);
+		sPush(theValue, iChanW);
+		}
+	}
+
+} // namespace ZooLib
---
+++ Portable/zoolib/PullPush_SeparatedValues.cpp
@@ -0,0 +1,117 @@
+/* -------------------------------------------------------------------------------------------------
+Copyright (c) 2018 Andrew Green
+http://www.zoolib.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------------------------- */
+
+#include "zoolib/PullPush_SeparatedValues.h"
+
+//#include "zoolib/Chan_UTF_string.h" // For ChanW_UTF_string8
+//#include "zoolib/Log.h"
+#include "zoolib/NameUniquifier.h" // For sName
+//#include "zoolib/ParseException.h"
+//#include "zoolib/Util_Chan.h"
+
+#include "zoolib/ZMACRO_foreach.h"
+
+#include <vector>
+
+namespace ZooLib {
+
+using namespace PullPush;
+using std::string;
+using std::vector;
+
+// =================================================================================================
+#pragma mark - Static parsing functions
+
+static bool spReadValues(const ChanR_UTF& iChanR, UTF32 iSeparator_Value, UTF32 iSeparator_Line, vector<string8>& oValues)
+	{
+	bool gotAny = false;
+	oValues.clear();
+	string8 curValue;
+	for (;;)
+		{
+		UTF32 theCP;
+		if (not sQRead(iChanR, theCP))
+			{
+			oValues.push_back(curValue);
+			break;
+			}
+
+		gotAny = true;
+
+		if (theCP == iSeparator_Line)
+			{
+			oValues.push_back(curValue);
+			break;
+			}
+
+		if (theCP == iSeparator_Value)
+			{
+			oValues.push_back(curValue);
+			curValue.clear();
+			}
+		else
+			{
+			curValue += theCP;
+			}
+		}
+	return gotAny;
+	}
+
+// =================================================================================================
+#pragma mark - Pull_SeparatedValues_Options
+
+Pull_SeparatedValues_Options::Pull_SeparatedValues_Options(
+	UTF32 iSeparator_Value, UTF32 iSeparator_Line)
+:	fSeparator_Value(iSeparator_Value)
+,	fSeparator_Line(iSeparator_Line)
+	{}
+
+// =================================================================================================
+#pragma mark -
+
+bool sPull_SeparatedValues_Push(const ChanR_UTF& iChanR,
+	const Pull_SeparatedValues_Options& iOptions,
+	const ChanW_Any& iChanW)
+	{
+	vector<string8> theNames;
+	if (not spReadValues(iChanR, iOptions.fSeparator_Value, iOptions.fSeparator_Line,
+		theNames))
+		{
+		return false;
+		}
+
+	for (;;)
+		{
+		vector<string8> theValues;
+		if (not spReadValues(iChanR, iOptions.fSeparator_Value, iOptions.fSeparator_Line,
+		 	theValues))
+			{
+			sPush(kEnd, iChanW);
+			return true;
+			}
+		for (size_t xx = 0; xx < theNames.size() && xx < theValues.size(); ++xx)
+			{
+			sPush(sName(theNames[xx]), iChanW);
+			sPush(theValues[xx], iChanW);
+			}
+		}
+	}
+
+} // namespace ZooLib
