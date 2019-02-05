b83d860ff990173eced87ab19fee28591dedf7cc
Andrew Green <ag@em.net>
2019-02-05 09:20:48 -0600

    Cleanup, give the Rename field a more descriptive name.

--- Project/zoolib/RelationalAlgebra/Transform_ConsolidateRenames.cpp
+++ Project/zoolib/RelationalAlgebra/Transform_ConsolidateRenames.cpp
@@ -22,7 +22,6 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "zoolib/Expr/Visitor_Expr_Op_Do_Transform_T.h"
 
-//#include "zoolib/RelationalAlgebra/Expr_Rel_Embed.h"
 #include "zoolib/RelationalAlgebra/Expr_Rel_Rename.h"
 #include "zoolib/RelationalAlgebra/Transform_ConsolidateRenames.h"
 
@@ -36,7 +35,6 @@ using namespace Util_STL;
 
 class Transform_ConsolidateRenames
 :	public virtual Visitor_Expr_Op_Do_Transform_T<Expr_Rel>
-//##,	public virtual Visitor_Expr_Rel_Embed
 ,	public virtual Visitor_Expr_Rel_Rename
 	{
 	typedef Visitor_Expr_Op_Do_Transform_T<Expr_Rel> inherited;
@@ -48,47 +46,48 @@ public:
 
 // From Visitor_Expr_Rel_XXX
 	virtual void Visit_Expr_Rel_Rename(const ZRef<Expr_Rel_Rename>& iExpr);
-//##	virtual void Visit_Expr_Rel_Embed(const ZRef<Expr_Rel_Embed>& iExpr);
 
-	Rename fRename;
+	Rename fRename_LeafToRoot;
 	};
 
 void Transform_ConsolidateRenames::Visit_Expr_Op0(const ZRef<Expr_Op0_T<Expr_Rel> >& iExpr)
 	{
-	SaveSetRestore<Rename> ssr(fRename, Rename());
+	SaveSetRestore<Rename> ssr(fRename_LeafToRoot, Rename());
 	inherited::Visit_Expr_Op0(iExpr);
 	}
 
 void Transform_ConsolidateRenames::Visit_Expr_Op1(const ZRef<Expr_Op1_T<Expr_Rel> >& iExpr)
 	{
-	SaveSetRestore<Rename> ssr(fRename, Rename());
+	SaveSetRestore<Rename> ssr(fRename_LeafToRoot, Rename());
 	inherited::Visit_Expr_Op1(iExpr);
 	}
 
 void Transform_ConsolidateRenames::Visit_Expr_Op2(const ZRef<Expr_Op2_T<Expr_Rel> >& iExpr)
 	{
-	SaveSetRestore<Rename> ssr(fRename, Rename());
+	SaveSetRestore<Rename> ssr(fRename_LeafToRoot, Rename());
 	inherited::Visit_Expr_Op2(iExpr);
 	}
 
 void Transform_ConsolidateRenames::Visit_Expr_Rel_Rename(const ZRef<Expr_Rel_Rename>& iExpr)
 	{
+	SaveRestore<Rename> sr(fRename_LeafToRoot);
+
 	string8 newName = iExpr->GetNew();
 
-	if (ZQ<string8> theQ = sQGetErase(fRename, newName))
+	if (ZQ<string8> theQ = sQGetErase(fRename_LeafToRoot, newName))
 		newName = *theQ;
 
 	const string8 oldName = iExpr->GetOld();
 
-	sInsertMust(fRename, oldName, newName);
+	sInsertMust(fRename_LeafToRoot, oldName, newName);
 
 	ZRef<Expr_Rel> newOp0 = this->Do(iExpr->GetOp0());
 
-	if (ZQ<string8> theQ = sQGet(fRename, oldName))
+	if (newName != oldName)
 		{
-		if (*theQ == newName)
+		if (ZQ<string8> theQ = sQGet(fRename_LeafToRoot, oldName))
 			{
-			if (newName != oldName)
+			if (*theQ == newName)
 				newOp0 = sRename(newOp0, newName, oldName);
 			}
 		}
