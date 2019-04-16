c5a903739f337ca2f99a7ef08423f3620348b5b1
Andrew Green <ag@em.net>
2019-04-16 11:13:39 -0500

    Make FinderHider visible if DebugFlags::sTouches is set.

--- Project/zoolib/GameEngine/Util_FinderHider.cpp
+++ Project/zoolib/GameEngine/Util_FinderHider.cpp
@@ -1,5 +1,7 @@
 #include "zoolib/GameEngine/Util_FinderHider.h"
 
+#include "zoolib/GameEngine/DebugFlags.h"
+
 namespace ZooLib {
 namespace GameEngine {
 namespace Util {
@@ -17,27 +19,32 @@ ZRef<Rendered> sFinderHider(
 	const Rat iWidth = X(iGameSize);
 	const Rat iHeight = Y(iGameSize);
 
+	RGBA theColor = sRGBA(0, 1);
+
+	if (DebugFlags::sTouches)
+		theColor  = sRGBA(0.25,1);
+
 	// Left
 	finderHider->Append(sRendered_Rect(
-		sRGBA(0,1), sRect<GRect>(-1000, -1000, 0, iHeight+1000)));
+		theColor, sRect<GRect>(-1000, -1000, 0, iHeight+1000)));
 
 	// Top
 	finderHider->Append(sRendered_Rect(
-		sRGBA(0,1), sRect<GRect>(-1000, -1000, iWidth+1000, 0)));
+		theColor, sRect<GRect>(-1000, -1000, iWidth+1000, 0)));
 
 	// Right
 	finderHider->Append(sRendered_Rect(
-		sRGBA(0,1), sRect<GRect>(iWidth, -1000, iWidth+1000, iHeight+1000)));
+		theColor, sRect<GRect>(iWidth, -1000, iWidth+1000, iHeight+1000)));
 
 	// Bottom
 	finderHider->Append(sRendered_Rect(
-		sRGBA(0,1), sRect<GRect>(-1000, iHeight, iWidth+1000, iHeight+1000)));
+		theColor, sRect<GRect>(-1000, iHeight, iWidth+1000, iHeight+1000)));
 	
 	ZRef<Rendered_Group> theGroup = sRendered_Group();
 	theGroup->Append(iRendered);
 
 	// Bring it fairly far forward, but well behind frontmost (which is at 128)
-	theGroup->Append(sRendered_Mat(sTranslate3Z<Rat>(20), finderHider));
+		theGroup->Append(sRendered_Mat(sTranslate3Z<Rat>(20), finderHider));
 
 	return theGroup;
 	}
