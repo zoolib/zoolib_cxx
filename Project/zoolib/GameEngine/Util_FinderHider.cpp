77fa436881c82ce2f3297509295173b2b74dd98a
Andrew Green <ag@em.net>
2019-05-25 17:07:13 -0500

    Simplify.

--- Project/zoolib/GameEngine/Util_FinderHider.cpp
+++ Project/zoolib/GameEngine/Util_FinderHider.cpp
@@ -14,39 +14,35 @@ ZRef<Rendered> sFinderHider(
 	GPoint iBackingSize,
 	GPoint iGameSize)
 	{
+	// Rectangles to mask off extraneous crud.
 	ZRef<Rendered_Group> finderHider = sRendered_Group();
 
 	const Rat iWidth = X(iGameSize);
 	const Rat iHeight = Y(iGameSize);
 
-	RGBA theColor = sRGBA(0, 1);
-
-	if (DebugFlags::sTouches)
-		theColor  = sRGBA(0.25,1);
-
 	// Left
-	finderHider->Append(sRendered_Rect(
-		theColor, sRect<GRect>(-1000, -1000, 0, iHeight+1000)));
+	finderHider->Append(sRendered_Rect(sRect<GRect>(-1000, -1000, 0, iHeight+1000)));
 
 	// Top
-	finderHider->Append(sRendered_Rect(
-		theColor, sRect<GRect>(-1000, -1000, iWidth+1000, 0)));
+	finderHider->Append(sRendered_Rect(sRect<GRect>(-1000, -1000, iWidth+1000, 0)));
 
 	// Right
-	finderHider->Append(sRendered_Rect(
-		theColor, sRect<GRect>(iWidth, -1000, iWidth+1000, iHeight+1000)));
+	finderHider->Append(sRendered_Rect(sRect<GRect>(iWidth, -1000, iWidth+1000, iHeight+1000)));
 
 	// Bottom
-	finderHider->Append(sRendered_Rect(
-		theColor, sRect<GRect>(-1000, iHeight, iWidth+1000, iHeight+1000)));
-	
-	ZRef<Rendered_Group> theGroup = sRendered_Group();
-	theGroup->Append(iRendered);
+	finderHider->Append(sRendered_Rect(sRect<GRect>(-1000, iHeight, iWidth+1000, iHeight+1000)));
 
-	// Bring it fairly far forward, but well behind frontmost (which is at 128)
-		theGroup->Append(sRendered_Mat(sTranslate3Z<Rat>(20), finderHider));
+	const RGBA theColor = DebugFlags::sTouches ? sRGBA(0.25,1) : sRGBA(0, 1);
 
-	return theGroup;
+	ZRef<Rendered_Group> result = sRendered_Group();
+
+	// include what we were passed
+	result->Append(iRendered);
+
+	// Apply color to the finderHider and bring it fairly far forward, behind frontmost at 128
+	result->Append(sRendered_Blush(theColor, sRendered_Mat(sTranslate3Z<Rat>(20), finderHider)));
+
+	return result;
 	}
 
 } // namespace Util
