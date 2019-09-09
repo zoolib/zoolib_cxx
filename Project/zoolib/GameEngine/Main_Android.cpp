09ccb2a1a8ae978522edbe7bf9c69f2230f772b9
Andrew Green <ag@em.net>
2019-09-08 22:13:13 -0500

    Move what had been statics into instance vars.

--- Project/zoolib/GameEngine/Main_Android.cpp
+++ Project/zoolib/GameEngine/Main_Android.cpp
@@ -22,31 +22,12 @@
 // MARK: -
 
 using std::map;
+using std::string;
 using std::vector;
 
 using namespace ZooLib;
 using namespace ZooLib::GameEngine;
-
-// =================================================================================================
-// MARK: -
-
-namespace { // anonymous
-
-ZMtx sMtx;
-
-struct TouchInfo
-	{
-    int fPointerId;
-	int fAction;
-	int fX;
-	int fY;
-	};
-
-vector<TouchInfo> sTouches;
-
-typedef map<int64,ZP<Touch> > TouchMap;
-
-} // anonymous namespace
+using namespace ZooLib::Util_STL;
 
 // =================================================================================================
 
@@ -66,182 +47,207 @@ void JNI_OnUnload(JavaVM* vm, void* reserved)
 
 // =================================================================================================
 
+class GameIntermediary
+	{
+	ZMtx fMtx;
+
+	struct TouchInfo
+		{
+		int fPointerId;
+		int fAction;
+		int fX;
+		int fY;
+		};
+
+	vector<TouchInfo> fTouches;
+
+	typedef map<int64,ZP<Touch> > TouchMap;
+
+	TouchMap fTouchesAll;
+
+public:
+	ZP<GameEngine::Game> fGame;
+
+	GameIntermediary(const string& iAPK)
+		{
+		ZP<SoundMeister> theSoundMeister;
+		ZP<Callable_Void> theHaptic;
+
+		FileSpec resourceFS = sFileSpec_Zip(iAPK).Child("assets");
+
+		fGame = GameEngine::sMakeGame(resourceFS, false, theSoundMeister, theHaptic);
+		}
+
+	virtual ~GameIntermediary()
+		{}
+
+	void OnTouch(jint pointerId, jint action, jfloat x, jfloat y, jfloat p)
+		{
+		if (ZLOGF(w, eErr))
+			w << "pointerId: " << pointerId << ", action: " << action << ", x: " << x << ", y: " << y << ", p: " << p;
+
+		ZAcqMtx axq(fMtx);
+		TouchInfo theTI;
+
+		theTI.fPointerId= pointerId;
+		theTI.fAction = action;
+		theTI.fX = x;
+		theTI.fY = y;
+
+		fTouches.push_back(theTI);
+		}
+
+	void UpdateTouches(const GPoint& iPixelSize)
+		{
+		const GPoint theGameSize = fGame->GetGameSize();
+
+		TouchSet theTouchesDown;
+		TouchSet theTouchesMove;
+		TouchSet theTouchesUp;
+
+		ZAcqMtx acq(fMtx);
+		foreachv (const TouchInfo& theTI, fTouches)
+			{
+			ZP<Touch> theTouch;
+			if (ZQ<ZP<Touch> > theQ = sQGet(fTouchesAll, theTI.fPointerId))
+				{
+				theTouch = *theQ;
+				}
+			else
+				{
+				theTouch = new Touch;
+				theTouch->fFingerID = theTI.fPointerId;
+				sInsertMust(fTouchesAll, theTI.fPointerId, theTouch);
+				}
+
+			theTouch->fPos = sHomogenous(GameEngine::sPixelToGame(
+				iPixelSize, theGameSize, sGPoint(theTI.fX, theTI.fY)));
+
+			enum
+				{
+				ACTION_DOWN = 0,
+				ACTION_UP = 1,
+				ACTION_MOVE = 2,
+				ACTION_CANCEL = 3,
+				ACTION_OUTSIDE = 4,
+				ACTION_POINTER_DOWN = 5,
+				ACTION_POINTER_UP = 6,
+				};
+
+			switch (theTI.fAction)
+				{
+				case ACTION_DOWN:
+				case ACTION_POINTER_DOWN:
+					{
+					sInsertMust(theTouchesDown, theTouch);
+					break;
+					}
+				case ACTION_UP:
+				case ACTION_POINTER_UP:
+				case ACTION_CANCEL:
+					{
+					sInsertMust(theTouchesUp, theTouch);
+					sEraseMust(fTouchesAll, theTI.fPointerId);
+					break;
+					}
+				case ACTION_MOVE:
+				case ACTION_OUTSIDE:
+					{
+					// The same touch may well have multiple moves reported, so we don't sInsertMust here.
+					sQInsert(theTouchesMove, theTouch);
+					break;
+					}
+				}
+			}
+		fTouches.clear();
+
+		fGame->UpdateTouches(&theTouchesDown, &theTouchesMove, &theTouchesUp);
+		}
+	};
+
+
+// =================================================================================================
+
 #pragma clang diagnostic ignored "-Wmissing-prototypes"
 
 #define JNIDEF(aa) extern "C" JNIEXPORT aa JNICALL
 
 JNIDEF(jlong)
 Java_org_zoolib_ViewModel_1Game_nspInit(JNIEnv *env, jclass iClass, jstring iAPKPath)
-    {
+	{
 	JNI::Env theEnv(env);
-
-	ZP<Callable_Void> theHaptic;
-
-    FileSpec resourceFS = sFileSpec_Zip(JNI::sAsString(iAPKPath)).Child("assets");
-
-    ZP<GameEngine::Game> theGame = GameEngine::sMakeGame(resourceFS, false, null, theHaptic);
-    theGame->Retain();
-	return reinterpret_cast<jlong>(theGame.Get());
-    }
+	return reinterpret_cast<jlong>(new GameIntermediary(JNI::sAsString(iAPKPath)));
+	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npPauseGameLoop(JNIEnv *env, jobject ob,
 	jlong iNative)
-    {
+	{
 	JNI::Env theEnv(env);
 	ZLOGTRACE(eDebug);
-	if (ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative))
-		theGame->Pause();
-    }
+	if (GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative))
+		theGI->fGame->Pause();
+	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npResumeGameLoop(JNIEnv *env, jobject ob,
 	jlong iNative)
-    {
+	{
 	JNI::Env theEnv(env);
 	ZLOGTRACE(eDebug);
-	if (ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative))
-		theGame->Resume();
-    }
+	if (GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative))
+		theGI->fGame->Resume();
+	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npTearDown(JNIEnv *env, jobject ob,
 	jlong iNative)
-    {
+	{
 	JNI::Env theEnv(env);
 	ZLOGTRACE(eDebug);
-	if (ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative))
-		theGame->Release();
-    }
+	if (GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative))
+		delete theGI;
+	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npSurfaceAndContextIsFresh(JNIEnv *env, jobject ob,
 	jlong iNative)
-    {
+	{
 	JNI::Env theEnv(env);
 	ZLOGTRACE(eDebug);
-	if (ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative))
-		theGame->ExternalPurgeHasOccurred();
-    }
+	if (GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative))
+		theGI->fGame->ExternalPurgeHasOccurred();
+	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npOnTouch(
-    JNIEnv *env, jobject ob,
-    jlong iNative,
-    jint pointerId, jint action,
-    jfloat x, jfloat y, jfloat p)
-    {
+	JNIEnv *env, jobject ob,
+	jlong iNative,
+	jint pointerId, jint action,
+	jfloat x, jfloat y, jfloat p)
+	{
 	JNI::Env theEnv(env);
 
-// 	if (ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative))
-
-	if (ZLOGF(w, eErr))
-		w << "pointerId: " << pointerId << ", action: " << action << ", x: " << x << ", y: " << y << ", p: " << p;
-
-	ZAcqMtx axq(sMtx);
-	TouchInfo theTI;
-
-	theTI.fPointerId= pointerId;
-	theTI.fAction = action;
-	theTI.fX = x;
-	theTI.fY = y;
-
-	sTouches.push_back(theTI);
-    }
-
-static TouchMap spTouchesAll;
-
-static void spUpdateTouches(
-	const GPoint& iPixelSize, const GPoint& iGameSize,
-	TouchMap& ioAll,
-	TouchSet& ioDown,
-	TouchSet& ioMove,
-	TouchSet& ioUp)
-	{
-	using namespace Util_STL;
-
-	ZAcqMtx acq(sMtx);
-	foreachv (const TouchInfo& theTI, sTouches)
-		{
-		ZP<Touch> theTouch;
-		if (ZQ<ZP<Touch> > theQ = sQGet(ioAll, theTI.fPointerId))
-			{
-			theTouch = *theQ;
-			}
-		else
-			{
-			theTouch = new Touch;
-			theTouch->fFingerID = theTI.fPointerId;
-			sInsertMust(ioAll, theTI.fPointerId, theTouch);
-			}
-
-		theTouch->fPos = sHomogenous(GameEngine::sPixelToGame(
-			iPixelSize, iGameSize, sGPoint(theTI.fX, theTI.fY)));
-
-        enum
-            {
-            ACTION_DOWN = 0,
-            ACTION_UP = 1,
-            ACTION_MOVE = 2,
-            ACTION_CANCEL = 3,
-            ACTION_OUTSIDE = 4,
-            ACTION_POINTER_DOWN = 5,
-            ACTION_POINTER_UP = 6,
-            };
-
-        switch (theTI.fAction)
-        	{
-            case ACTION_DOWN:
-            case ACTION_POINTER_DOWN:
-            	{
-				sInsertMust(ioDown, theTouch);
-				break;
-				}
-            case ACTION_UP:
-            case ACTION_POINTER_UP:
-            case ACTION_CANCEL:
-            	{
-            	sInsertMust(ioUp, theTouch);
-				sEraseMust(ioAll, theTI.fPointerId);
-				break;
-				}
-            case ACTION_MOVE:
-            case ACTION_OUTSIDE:
-            	{
-            	// The same touch may well have multiple moves reported, so we don't sInsertMust here.
-            	sQInsert(ioMove, theTouch);
-				break;
-				}
-			}
-		}
-	sTouches.clear();
+	GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative);
+	theGI->OnTouch(pointerId, action, x, y, p);
 	}
 
 JNIDEF(void)
 Java_org_zoolib_ViewModel_1Game_npDraw(
-    JNIEnv *env, jobject ob,
-    jlong iNative,
-    jint width, jint height)
-    {
+	JNIEnv *env, jobject ob,
+	jlong iNative,
+	jint width, jint height)
+	{
 	JNI::Env theEnv(env);
 
-	ZP<GameEngine::Game> theGame = reinterpret_cast<GameEngine::Game*>(iNative);
+	GameIntermediary* theGI = reinterpret_cast<GameIntermediary*>(iNative);
 
-    const double loopStart = Time::sSystem();
+	const double loopStart = Time::sSystem();
 
-    const GPoint backingSize = sGPoint(width, height);
+	const GPoint backingSize = sGPoint(width, height);
 
-    theGame->Draw(loopStart, backingSize, null);
-
-    TouchSet theTouchesDown;
-    TouchSet theTouchesMove;
-    TouchSet theTouchesUp;
-
-    spUpdateTouches(backingSize, theGame->GetGameSize(),
-        spTouchesAll,
-        theTouchesDown, theTouchesMove, theTouchesUp);
-
-    theGame->UpdateTouches(&theTouchesDown, &theTouchesMove, &theTouchesUp);
-    }
+	theGI->fGame->Draw(loopStart, backingSize, null);
+	theGI->UpdateTouches(backingSize);
+	}
 
 #endif // defined(__ANDROID__)
