4069c2e415c958db0344839717d23915e2766ae4
Andrew Green <ag@em.net>
2020-06-09 12:18:21 -0500

    Simplify, now we require C++11.

--- Core/zoolib/Compat_unordered_map.h
+++ Core/zoolib/Compat_unordered_map.h
@@ -1,54 +1,16 @@
-/* -------------------------------------------------------------------------------------------------
-Copyright (c) 2012 Andrew Green
-http://www.zoolib.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-and associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
-OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-------------------------------------------------------------------------------------------------- */
+// Copyright (c) 2012-2020 Andrew Green. MIT License. http://www.zoolib.org
 
 #ifndef __ZooLib_Compat_unordered_map_h__
 #define __ZooLib_Compat_unordered_map_h__ 1
 #include "zconfig.h"
-#include "zoolib/ZCONFIG_SPI_Definitions.h"
 
-#ifndef ZCONFIG_SPI_Desired__unordered_map
-	#define ZCONFIG_SPI_Desired__unordered_map 1
-#endif
+#include <unordered_map>
 
-#if ZCONFIG_SPI_Desired__unordered_map
+namespace ZooLib {
 
-	#if ZMACRO_Has_tr1
-		#define ZCONFIG_SPI_Avail__unordered_map 1
-		#include ZMACRO_tr1_header(unordered_map)
-		namespace ZooLib
-			{
-			using ZMACRO_namespace_tr1_prefix::unordered_map;
-			using ZMACRO_namespace_tr1_prefix::unordered_multimap;
-			}
-	#elif defined(__GNUC__)
-		#include <hash_map>
-		namespace ZooLib
-			{
-			typedef hash_map unordered_map;
-			typedef unordered_hash_map unordered_multimap;
-			}
-	#endif
-#endif
+using std::unordered_map;
+using std::unordered_multimap;
 
-#ifndef ZCONFIG_SPI_Avail__unordered_map
-	#define ZCONFIG_SPI_Avail__unordered_map 0
-#endif
+} // namespace ZooLib
 
 #endif // __ZooLib_Compat_unordered_map_h__
