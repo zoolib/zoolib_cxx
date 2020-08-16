dd2f7c33eea4ce4f975511a945aa5a1993f39563
Andrew Green <ag@em.net>
2020-08-16 14:37:51 -0500

    WS and comments.

--- Core/zoolib/ChanW_Bin.h
+++ Core/zoolib/ChanW_Bin.h
@@ -1,22 +1,4 @@
-/* -------------------------------------------------------------------------------------------------
-Copyright (c) 2014 Andrew Green
-http://www.zoolib.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-and associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES
-OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-------------------------------------------------------------------------------------------------- */
+// Copyright (c) 2014-2020 Andrew Green. MIT License. http://www.zoolib.org
 
 #ifndef __ZooLib_ChanW_Bin_h__
 #define __ZooLib_ChanW_Bin_h__ 1
@@ -28,7 +10,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 namespace ZooLib {
 
 // =================================================================================================
-#pragma mark -
+#pragma mark - ChanW_Bin
 
 typedef ChanW<byte> ChanW_Bin;
 
@@ -46,6 +28,9 @@ size_t sWriteMemFully(const ChanW_Bin& iChan, const void* iSource, size_t iCount
 inline
 void sEWriteMem(const ChanW_Bin& iChan, const void* iSource, size_t iCount)
 	{ return sEWrite<byte>(iChan, static_cast<const byte*>(iSource), iCount); }
+
+// =================================================================================================
+#pragma mark -
 
 template <class T>
 bool sQWriteNative(const ChanW_Bin& iChanW, const T& iT)
